1933de40befd0471d6d05db3129d6360e089c543
fonhorst <fonhorst@alipoov.nb@gmail.com>
2021-12-05 22:31:18 +0300

    broken test for the weighted blender

--- tests/spark/unit/test_blenders/test_blenders.py
+++ tests/spark/unit/test_blenders/test_blenders.py
@@ -4,9 +4,10 @@ from typing import cast
 import pandas as pd
 from pyspark.sql import SparkSession
 
-from lightautoml.automl.blend import BestModelSelector
+from lightautoml.automl.blend import BestModelSelector, Blender, WeightedBlender
 from lightautoml.dataset.np_pd_dataset import NumpyDataset
-from lightautoml.spark.automl.blend import BestModelSelector as SparkBestModelSelector
+from lightautoml.spark.automl.blend import BestModelSelector as SparkBestModelSelector, \
+    WeightedBlender as SparkWeightedBlender
 from lightautoml.spark.dataset.base import SparkDataset
 from lightautoml.spark.ml_algo.boost_lgbm import BoostLGBM as SparkBoostLGBM
 from lightautoml.spark.ml_algo.linear_pyspark import LinearLBFGS as SparkLinearLBFGS
@@ -16,7 +17,7 @@ from lightautoml.tasks import Task
 from .. import from_pandas_to_spark, spark, compare_obtained_datasets
 
 
-def test_best_blender(spark: SparkSession):
+def do_compare_blenders(spark: SparkSession, lama_blender: Blender, spark_blender: Blender):
     with open("../resources/datasets/dump_tabular_automl_lgb_linear/Lpred_0_before_blender_before_blender.pickle", "rb") as f:
         data_1, target_1, features_1, roles_1 = pickle.load(f)
         target_1 = pd.Series(target_1)
@@ -48,13 +49,6 @@ def test_best_blender(spark: SparkSession):
     pipes[0].ml_algos = [linear_l2_model]
     pipes[1].ml_algos = [lgbm_model]
 
-    # general_params = {
-    #     "use_algos": ["lgb", "linear_l2"]
-    # }
-
-    lama_blender = BestModelSelector()
-    spark_blender = SparkBestModelSelector()
-
     lama_ds, _ = lama_blender.fit_predict(lama_level_preds, pipes)
     spark_ds, _ = spark_blender.fit_predict(spark_level_preds, pipes)
 
@@ -62,3 +56,11 @@ def test_best_blender(spark: SparkSession):
     spark_ds = cast(SparkDataset, spark_ds)
 
     compare_obtained_datasets(lama_ds, spark_ds)
+
+
+def test_best_blender(spark: SparkSession):
+    do_compare_blenders(spark, BestModelSelector(), SparkBestModelSelector())
+
+
+def test_weighted_blender(spark: SparkSession):
+    do_compare_blenders(spark, WeightedBlender(), SparkWeightedBlender())
