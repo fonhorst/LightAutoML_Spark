f26f1ffed11f46b4956f51dc8eda42aec9804e80
fonhorst <fonhorst@alipoov.nb@gmail.com>
2021-12-10 14:11:45 +0300

    ---

--- docker/spark-lama.dockerfile
+++ docker/spark-lama.dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8.6
+FROM python:3.9.9
 
 RUN pip install poetry
 WORKDIR /code
@@ -16,4 +16,4 @@ RUN ln -s /usr/local/lib/jdk-11/bin/java /usr/local/bin/java
 COPY . /code
 RUN poetry build
 RUN pip install dist/LightAutoML-0.3.0-py3-none-any.whl
-COPY ivy2_cache /root/.ivy2/cache
+#COPY ivy2_cache /root/.ivy2/cache
--- examples/spark/simple_tabular_classification_used_cars.py
+++ examples/spark/simple_tabular_classification_used_cars.py
@@ -3,8 +3,28 @@
 """
 Simple example for binary classification on tabular data.
 """
+import time
+
+import logging
+import sys
+
+formatter = logging.Formatter(
+    fmt='%(asctime)s %(name)s {%(module)s:%(lineno)d} %(levelname)s:%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %p'
+)
+# set up logging to console
+console = logging.StreamHandler(sys.stdout)
+console.setLevel(logging.DEBUG)
+console.setFormatter(formatter)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
+root_logger.addHandler(console)
+
+from contextlib import contextmanager
 
 import pandas as pd
+from pyspark.sql import SparkSession
 
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
@@ -14,10 +34,50 @@ from sklearn.model_selection import train_test_split
 # TODO: put a correct path for used_cars dataset
 from lightautoml.spark.automl.presets.tabular_presets import TabularAutoML
 from lightautoml.spark.tasks.base import Task as SparkTask
-from lightautoml.spark.utils import spark_session, print_exec_time
+from lightautoml.spark.utils import print_exec_time
+
+loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict if name.startswith('lightautoml')]
+for logger in loggers:
+    logger.setLevel(logging.INFO)
+    # logger.addHandler(console)
 
 data = pd.read_csv("examples/data/tiny_used_cars_data.csv")
 train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+
+
+@contextmanager
+def spark_session(parallelism: int = 1) -> SparkSession:
+    spark = (
+        SparkSession
+        .builder
+        .appName("SPARK-LAMA-app")
+        .master("spark://node4.bdcl:7077")
+        # .master("local[4]")
+        .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:0.9.4")
+        .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven")
+        .config("spark.driver.host", "node4.bdcl")
+        .config("spark.driver.cores", "4")
+        .config("spark.driver.memory", "16g")
+        .config("spark.cores.max", "12")
+        .config("spark.executor.instances", "3")
+        .config("spark.executor.memory", "16g")
+        .config("spark.executor.cores", "4")
+        .config("spark.memory.fraction", "0.6")
+        .config("spark.memory.storageFraction", "0.5")
+        .config("spark.sql.autoBroadcastJoinThreshold", "100MB")
+        .config("spark.sql.execution.arrow.pyspark.enabled", "false")
+        .getOrCreate()
+    )
+
+    print(f"Spark WebUI url: {spark.sparkContext.uiWebUrl}")
+
+    # time.sleep(600)
+    try:
+        yield spark
+    finally:
+        # time.sleep(600)
+        spark.stop()
+
 
 # run automl
 if __name__ == "__main__":
