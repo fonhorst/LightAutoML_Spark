b03c92672b8ac4304981ba583597d3b61000613a
Teryoshkin Sergey <se.teryoshkin@gmail.com>
2021-12-15 04:22:34 +0300

    Raw implementation

--- examples/spark/simple_tabular_classification_used_cars_spark_frame.py
+++ examples/spark/simple_tabular_classification_used_cars_spark_frame.py
@@ -9,6 +9,9 @@ import time
 import logging
 import sys
 
+from lightautoml.spark.dataset.base import SparkDataset
+from pyspark.sql import functions as F
+
 formatter = logging.Formatter(
     fmt='%(asctime)s %(name)s {%(module)s:%(lineno)d} %(levelname)s:%(message)s',
     datefmt='%Y-%m-%d %H:%M:%S %p'
@@ -49,18 +52,19 @@ def spark_session() -> SparkSession:
         SparkSession
         .builder
         .appName("SPARK-LAMA-app")
-        .master("spark://node4.bdcl:7077")
-        .config("spark.driver.host", "node4.bdcl")
-        .config("spark.driver.cores", "4")
-        .config("spark.driver.memory", "16g")
-        .config("spark.cores.max", "16")
-        .config("spark.executor.instances", "4")
-        .config("spark.executor.memory", "16g")
-        .config("spark.executor.cores", "4")
+        #.master("spark://node4.bdcl:7077")
+        .master("local")
+        #.config("spark.driver.host", "node4.bdcl")
+        #.config("spark.driver.cores", "4")
+        #.config("spark.driver.memory", "16g")
+        #.config("spark.cores.max", "16")
+        #.config("spark.executor.instances", "4")
+        #.config("spark.executor.memory", "16g")
+        #.config("spark.executor.cores", "4")
         .config("spark.memory.fraction", "0.6")
         .config("spark.memory.storageFraction", "0.5")
         .config("spark.sql.autoBroadcastJoinThreshold", "100MB")
-        .config("spark.sql.execution.arrow.pyspark.enabled", "false")
+        .config("spark.sql.execution.arrow.pyspark.enabled", "true")
         .getOrCreate()
     )
 
@@ -79,10 +83,13 @@ if __name__ == "__main__":
     with spark_session() as spark:
         task = SparkTask("reg")
 
-        # data = spark.read.csv(os.path.join("file://", os.getcwd(), "../data/tiny_used_cars_data.csv"), header=True, escape="\"")
-        data = spark.read.csv(os.path.join("file:///spark_data/tiny_used_cars_data.csv"), header=True, escape="\"")
+        data = spark.read.csv("tiny_used_cars_data_cleaned.csv", header=True, escape="\"")
         data = data.cache()
         train_data, test_data = data.randomSplit([0.8, 0.2], seed=42)
+
+        test_data_dropped = test_data \
+                                .withColumn(SparkDataset.ID_COLUMN, F.monotonically_increasing_id()) \
+                                .drop(F.col("price")).cache()
 
         automl = TabularAutoML(
             spark=spark,
@@ -97,3 +104,10 @@ if __name__ == "__main__":
                 train_data,
                 roles={"target": "price", "drop": ["dealer_zip", "description", "listed_date", "year"]}
             )
+
+        te_pred = automl.predict(test_data_dropped)
+
+        test_data_pd = test_data.toPandas()
+        te_pred_pd = te_pred.data.limit(50).toPandas()
+
+        _a = 1  # Just to check results in the debugger
