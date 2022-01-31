e6b4db836bc4d7fc95aa4f93aabb7bc0dd5e7b78
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-02-01 00:55:00 +0300

    minor refactoring to add more datasets easily

--- dev-tools/performance_tests/launch.py
+++ dev-tools/performance_tests/launch.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+from copy import deepcopy
 from pprint import pprint
 from typing import Any, Callable
 from lama_used_cars import calculate_automl as lama_automl
@@ -7,25 +8,47 @@ from lightautoml.spark.utils import logging_config, VERBOSE_LOGGING_FORMAT
 from spark_used_cars import calculate_automl as spark_automl
 
 
+USED_CARS_DATASET_ROLES = {
+    "target": "price",
+    "drop": ["dealer_zip", "description", "listed_date",
+             "year", 'Unnamed: 0', '_c0',
+             'sp_id', 'sp_name', 'trimId',
+             'trim_name', 'major_options', 'main_picture_url',
+             'interior_color', 'exterior_color'],
+    "numeric": ['latitude', 'longitude', 'mileage']
+}
+
+
 logging.config.dictConfig(logging_config(level=logging.INFO, log_filename='/tmp/lama.log'))
 logging.basicConfig(level=logging.DEBUG, format=VERBOSE_LOGGING_FORMAT)
 logger = logging.getLogger(__name__)
 
 
-def calculate_quality(calc_automl: Callable[[str, int, Any], dict]):
-    # dataset_path = "examples/data/tiny_used_cars_data.csv"
-    dataset_path = "examples/data/small_used_cars_data.csv"
-    # dataset_path = "/opt/0125x_cleaned.csv"
-    use_algos = ["lgb"]
+def calculate_quality(calc_automl: Callable):
+    config = {
+        "path": "examples/data/small_used_cars_data.csv",
+        "task_type": "reg",
+        "metric_name": "mse",
+        "target_col": USED_CARS_DATASET_ROLES["target"],
+        "use_algos": ["lgb"],
+        "roles": USED_CARS_DATASET_ROLES,
+        "dtype": {
+            'fleet': 'str', 'frame_damaged': 'str',
+            'has_accidents': 'str', 'isCab': 'str',
+            'is_cpo': 'str', 'is_new': 'str',
+            'is_oemcpo': 'str', 'salvage': 'str', 'theft_title': 'str'
+        }
+    }
+
     # seeds = [1, 42, 100, 200, 333, 555, 777, 2000, 50000, 100500,
     #              200000, 300000, 1_000_000, 2_000_000, 5_000_000, 74909, 54179, 68572, 25425]
-    seeds = [50000, 100500,
-                     200000, 300000, 1_000_000, 2_000_000, 5_000_000, 74909, 54179, 68572, 25425]
 
-    # seeds = [1, 42, 100, 200]
+    seeds = [42]
     results = []
     for seed in seeds:
-        res = calc_automl(dataset_path, seed, use_algos)
+        cfg = deepcopy(config)
+        cfg['seed'] = seed
+        res = calc_automl(**cfg)
         results.append(res)
         logger.info(f"Result for seed {seed}: {res}")
 
@@ -39,5 +62,5 @@ def calculate_quality(calc_automl: Callable[[str, int, Any], dict]):
 
 
 if __name__ == "__main__":
-    # calculate_quality(lama_automl)
-    calculate_quality(spark_automl)
+    calculate_quality(lama_automl)
+    # calculate_quality(spark_automl)
--- dev-tools/performance_tests/spark_used_cars.py
+++ dev-tools/performance_tests/spark_used_cars.py
@@ -4,9 +4,9 @@
 Simple example for binary classification on tabular data.
 """
 import logging
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
-from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.evaluation import RegressionEvaluator, BinaryClassificationEvaluator, MulticlassClassificationEvaluator
 from pyspark.sql import functions as F
 from pyspark.sql.types import DoubleType
 
@@ -18,16 +18,19 @@ from lightautoml.spark.utils import log_exec_time, spark_session
 logger = logging.getLogger(__name__)
 
 
-def calculate_automl(path:str, seed: int = 42, use_algos = ("lgb", "linear_l2")) -> Dict[str, Any]:
+def calculate_automl(path: str,
+                     task_type: str,
+                     metric_name: str,
+                     target_col: str = 'target',
+                     seed: int = 42,
+                     use_algos = ("lgb", "linear_l2"),
+                     roles: Optional[Dict] = None,
+                     dtype: Optional[None] = None) -> Dict[str, Any]:
+    roles = roles if roles else {}
+
     with spark_session(master="local[4]") as spark:
-        target_col = 'price'
-        task = SparkTask("reg")
+        task = SparkTask(task_type)
         data = spark.read.csv(path, header=True, escape="\"")
-        # data = spark.read.csv("file:///spark_data/tiny_used_cars_data.csv", header=True, escape="\"")
-        # data = spark.read.csv("file:///spark_data/derivative_datasets/0125x_cleaned.csv", header=True, escape="\"")
-        # data = spark.read.csv("fтile:///spark_data/derivative_datasets/4x_cleaned.csv", header=True, escape="\"")
-        # data = spark.read.csv("file:///spark_data/derivative_datasets/2x_cleaned.csv", header=True, escape="\"")
-        # data = spark.read.csv("file:///opt/0125l_dataset.csv", header=True, escape="\"")
         data = data.withColumnRenamed(target_col, f"{target_col}_old") \
             .select('*', F.col(f"{target_col}_old").astype(DoubleType()).alias(target_col)).drop(f"{target_col}_old") \
             .withColumn(SparkDataset.ID_COLUMN, F.monotonically_increasing_id()) \
@@ -42,15 +45,7 @@ def calculate_automl(path:str, seed: int = 42, use_algos = ("lgb", "linear_l2"))
         with log_exec_time("spark-lama training"):
             oof_predictions = automl.fit_predict(
                 train_data,
-                roles={
-                    "target": target_col,
-                    "drop": ["dealer_zip", "description", "listed_date",
-                             "year", 'Unnamed: 0', '_c0',
-                             'sp_id', 'sp_name', 'trimId',
-                             'trim_name', 'major_options', 'main_picture_url',
-                             'interior_color', 'exterior_color'],
-                    "numeric": ['latitude', 'longitude', 'mileage']
-                }
+                roles=roles
             )
 
         logger.info("Predicting on out of fold")
@@ -61,8 +56,17 @@ def calculate_automl(path:str, seed: int = 42, use_algos = ("lgb", "linear_l2"))
             .select(SparkDataset.ID_COLUMN, target_col, oof_predictions.features[0])
         )
 
-        evaluator = RegressionEvaluator(predictionCol=oof_predictions.features[0], labelCol=target_col,
-                                        metricName="mse")
+        if task_type == "reg":
+            evaluator = RegressionEvaluator(predictionCol=oof_predictions.features[0], labelCol=target_col,
+                                            metricName=metric_name)
+        elif task_type == "binary":
+            evaluator = BinaryClassificationEvaluator(predictionCol=oof_predictions.features[0], labelCol=target_col,
+                                                      metricName=metric_name)
+        elif task_type == "multiclass":
+            evaluator = MulticlassClassificationEvaluator(predictionCol=oof_predictions.features[0], labelCol=target_col,
+                                                          metricName=metric_name)
+        else:
+            raise ValueError(f"Task type {task_type} is not supported")
 
         metric_value = evaluator.evaluate(oof_preds_for_eval)
         logger.info(f"{evaluator.getMetricName()} score for out-of-fold predictions: {metric_value}")
