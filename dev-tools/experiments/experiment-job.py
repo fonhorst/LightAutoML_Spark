b48df0be7867dbb5fbfa4b998679255452cb07bc
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-01-31 19:32:19 +0300

    refactored experiment-job

--- dev-tools/experiments/experiment-job.py
+++ dev-tools/experiments/experiment-job.py
@@ -1,43 +1,99 @@
 import itertools
-import yaml
 import subprocess
+import time
+from copy import deepcopy
+from typing import Iterable, List, Set, Dict, Any
 
-with open("./dev-tools/experiments/experiment-config.yaml", "r") as stream:
-    try:
-        config_data = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+import yaml
+from tqdm import tqdm
 
-repeat_rate = config_data["spark_quality_repeat_rate"]
 
-# Make all possible experiments
-keys_exps, values_exps = zip(*config_data["experiment_params"].items())
-experiments = [dict(zip(keys_exps, v)) for v in itertools.product(*values_exps)]
+def read_config(cfg_path: str) -> Dict:
+    with open(cfg_path, "r") as stream:
+        try:
+            config_data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config_data
 
-# Retrieve static Spark paramaters and concatenate them with experiment paramaters
-experiments_configs = []
-for item in experiments:
-    t_dict = dict(item)
-    t_dict["use_state_file"] = config_data["use_state_file"]
-    t_dict.update(config_data["spark_static_config"])
-    experiments_configs.append(t_dict)
+
+def generate_experiments(config_data: Dict) -> List[Dict[str, Any]]:
+    repeat_rate = config_data["spark_quality_repeat_rate"]
+    # Make all possible experiments
+    keys_exps, values_exps = zip(*config_data["experiment_params"].items())
+    experiments = [dict(zip(keys_exps, v)) for v in itertools.product(*values_exps)]
+
+    # Retrieve static Spark paramaters and concatenate them with experiment paramaters
+    experiments_configs = []
+    for item in experiments:
+        experiment = dict(item)
+        experiment["use_state_file"] = config_data["use_state_file"]
+        experiment.update(config_data["spark_static_config"])
+        for i in range(repeat_rate):
+            name = (
+                f"exper-{experiment['dataset_path'].translate(str.maketrans({'/': '-', '_': '-', '.': '-'}))}"
+                f"-{experiment['spark.executor.instances']}-{experiment['spark.executor.memory']}-n{i + 1}"
+            )
+            exp_instance = deepcopy(experiment)
+            exp_instance["name"] = name
+            experiments_configs.append(exp_instance)
+
+    return experiments_configs
+
 
 # Run subprocesses with Spark jobs
-for experiment in experiments_configs:
+# for i in range(repeat_rate):
+def run_experiments(experiments_configs: List[Dict[str, Any]]):
     processes = []
-    for i in range(repeat_rate):
-        name = (
-            f"exper-{experiment['dataset_path'].translate(str.maketrans({'/':'-', '_':'-', '.':'-'}))}"
-            f"-{experiment['spark.executor.instances']}-{experiment['spark.executor.memory']}-n{i+1}"
-        )
-        experiment["name"] = name
-
+    for experiment in experiments_configs:
+        name = experiment["name"]
         with open(f"/tmp/{name}-config.yaml", "w+") as outfile:
             yaml.dump(experiment, outfile, default_flow_style=False)
 
-        processes.append(subprocess.Popen(["./dev-tools/bin/test-job-run.sh", str(name)]))
+        p = subprocess.Popen(["./dev-tools/bin/test-job-run.sh", str(name)])
+        yield p
 
     for p in processes:
         p.wait()
 
-print("Finished processes")
+
+def limit_procs(it: Iterable[subprocess.Popen], max_parallel_ops: int = 1):
+    assert max_parallel_ops > 0
+
+    procs: Set[subprocess.Popen] = set()
+
+    def try_to_remove_finished():
+        for p in procs:
+            if p.poll():
+                procs.remove(p)
+                break
+
+    for el in it:
+        procs.add(el)
+        yield el
+
+        while len(procs) >= max_parallel_ops:
+            try_to_remove_finished()
+            time.sleep(5)
+
+
+def wait_for_all(procs: Iterable[subprocess.Popen]):
+    for p in procs:
+        p.wait()
+
+
+def main():
+    cfg_path = "./dev-tools/experiments/experiment-config.yaml"
+    cfg = read_config(cfg_path)
+    exp_cfgs = generate_experiments(cfg)
+    exp_procs = list(tqdm(
+        limit_procs(run_experiments(exp_cfgs), max_parallel_ops=2),
+        desc="Experiment",
+        total=len(exp_cfgs)
+    ))
+    wait_for_all(exp_procs)
+    print("Finished processes")
+
+
+if __name__ == "__main__":
+    main()
