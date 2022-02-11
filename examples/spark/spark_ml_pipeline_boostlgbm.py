f07ce7a5375c70588d93b3ff45a851a88369c431
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-02-11 14:42:28 +0300

    basic example with working boostlgbm on spark pipelines

--- examples/spark/spark_ml_pipeline_boostlgbm.py
+++ examples/spark/spark_ml_pipeline_boostlgbm.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 import logging
 import logging.config
+from typing import cast
 
 from pyspark.ml import Pipeline
 
@@ -44,7 +45,10 @@ if __name__ == "__main__":
         
         sdataset = sdataset_tmp.empty()
         sdataset.set_data(
-            sdataset_tmp.data.select(SparkDataset.ID_COLUMN, *feats_to_select_numeric),
+            sdataset_tmp.data
+                .select(SparkDataset.ID_COLUMN, *feats_to_select_numeric)
+                .join(sdataset.target, on=SparkDataset.ID_COLUMN)
+                .join(sdataset.folds, on=SparkDataset.ID_COLUMN),
             sdataset_tmp.features,
             sdataset_tmp.roles
         )
@@ -63,8 +67,9 @@ if __name__ == "__main__":
         iterator = DummyIterator(sdataset)
         spark_ml_algo = SparkBoostLGBM()
         spark_ml_algo, _ = tune_and_fit_predict(spark_ml_algo, DefaultTuner(), iterator)
-        spark_ml_algo.transformer.transform(sdataset.data)
+        spark_ml_algo = cast(SparkBoostLGBM, spark_ml_algo)
+        sdf = spark_ml_algo.transformer.transform(sdataset.data)
 
+        sdf.write.mode('overwrite').format('noop').save()
 
         logger.info("Finished")
-
