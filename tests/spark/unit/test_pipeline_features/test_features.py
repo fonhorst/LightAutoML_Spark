1658bbeefb513b36bfd114a9985b12bbb2218c0b
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-02-20 20:09:03 +0300

    all tests are passing on used_cars

--- tests/spark/unit/test_pipeline_features/test_features.py
+++ tests/spark/unit/test_pipeline_features/test_features.py
@@ -6,9 +6,11 @@ from pyspark.sql import SparkSession
 
 from lightautoml.dataset.np_pd_dataset import PandasDataset
 from lightautoml.dataset.roles import CategoryRole
+from lightautoml.pipelines.features.lgb_pipeline import LGBAdvancedPipeline, LGBSimpleFeatures
 from lightautoml.pipelines.features.linear_pipeline import LinearFeatures
 from lightautoml.reader.base import PandasToPandasReader
 from lightautoml.spark.dataset.base import SparkDataset
+from lightautoml.spark.pipelines.features.lgb_pipeline import SparkLGBAdvancedPipeline, SparkLGBSimpleFeatures
 from lightautoml.spark.pipelines.features.linear_pipeline import SparkLinearFeatures
 from lightautoml.tasks import Task
 from .. import DatasetForTest, spark as spark_sess
@@ -34,11 +36,44 @@ DATASETS = [
 ]
 
 
-@pytest.mark.parametrize("ds_config,cv", [(ds, 3) for ds in get_test_datasets(setting="fast")])
-def test_linear_features(spark: SparkSession, ds_config: Dict[str, Any], cv: int):
+def compare_feature_pipelines(spark: SparkSession, cv: int, ds_config: Dict[str, Any],
+                              lama_clazz, slama_clazz, ml_alg_kwargs: Dict[str, Any]):
     spark_ds = prepared_datasets(spark, cv, [ds_config], checkpoint_dir='/opt/test_checkpoints/')
     spark_ds = spark_ds[0]
 
+    # LAMA pipeline
+    pdf = pd.read_csv(ds_config['path'], dtype=ds_config['dtype'])
+    reader = PandasToPandasReader(task=Task(spark_ds.task.name), cv=cv, advanced_roles=False)
+    ds = reader.fit_read(pdf, roles=ds_config['roles'])
+
+    lama_pipeline = lama_clazz(**ml_alg_kwargs)
+    lama_feats = lama_pipeline.fit_transform(ds)
+    lf_pds = cast(PandasDataset, lama_feats.to_pandas())
+
+    # SLAMA pipeline
+    slama_pipeline = slama_clazz(**ml_alg_kwargs)
+    slama_pipeline.input_roles = spark_ds.roles
+    slama_feats = slama_pipeline.fit_transform(spark_ds)
+    slama_lf_pds = cast(PandasDataset, slama_feats.to_pandas())
+
+    # assert sorted(slama_pipeline.output_features) == sorted(lf_pds.features)
+    assert sorted(slama_pipeline.output_features) == sorted([f for f in lf_pds.features if not f.startswith('nanflg_')])
+    assert len(set(spark_ds.features).difference(slama_feats.features)) == 0
+    assert len(set(ds.features).difference(slama_feats.features)) == 0
+    assert set(slama_pipeline.output_roles.keys()) == set(f for f in lf_pds.roles.keys() if not f.startswith('nanflg_'))
+    assert all([(f in slama_feats.roles) for f in lf_pds.roles.keys() if not f.startswith('nanflg_')])
+
+    not_equal_roles = [
+        feat
+        for feat, prole in lf_pds.roles.items()
+        if not feat.startswith('nanflg_') and
+           not (type(prole) == type(slama_pipeline.output_roles[feat]) == type(slama_feats.roles[feat]))
+    ]
+    assert len(not_equal_roles) == 0, f"Roles are different: {not_equal_roles}"
+
+
+@pytest.mark.parametrize("ds_config,cv", [(ds, 3) for ds in get_test_datasets(setting="fast")])
+def test_linear_features(spark: SparkSession, ds_config: Dict[str, Any], cv: int):
     ml_alg_kwargs = {
         'auto_unique_co': 10,
         'max_intersection_depth': 3,
@@ -47,37 +82,26 @@ def test_linear_features(spark: SparkSession, ds_config: Dict[str, Any], cv: int
         'top_intersections': 4
     }
 
-    # LAMA pipeline
-    pdf = pd.read_csv(ds_config['path'],  dtype=ds_config['dtype'])
-    reader = PandasToPandasReader(task=Task(spark_ds.task.name), cv=cv, advanced_roles=False)
-    ds = reader.fit_read(pdf, roles=ds_config['roles'])
+    compare_feature_pipelines(spark, cv, ds_config, LinearFeatures, SparkLinearFeatures, ml_alg_kwargs)
 
-    linear_features = LinearFeatures(**ml_alg_kwargs)
-    dataset_feats = linear_features.fit_transform(ds)
-    lf_pds = cast(PandasDataset, dataset_feats.to_pandas())
 
-    # SLAMA pipeline
-    slama_linear_features = SparkLinearFeatures(**ml_alg_kwargs)
-    slama_linear_features.input_roles = spark_ds.roles
-    slama_dataset_feats = slama_linear_features.fit_transform(spark_ds)
-    slama_lf_pds = cast(PandasDataset, slama_dataset_feats.to_pandas())
+@pytest.mark.parametrize("ds_config,cv", [(ds, 3) for ds in get_test_datasets(setting="fast")])
+def test_lgbadv_features(spark: SparkSession, ds_config: Dict[str, Any], cv: int):
+    ml_alg_kwargs = {
+        'auto_unique_co': 10,
+        'max_intersection_depth': 3,
+        'multiclass_te_co': 3,
+        'output_categories': True,
+        'top_intersections': 4
+    }
 
-    # assert sorted(slama_linear_features.output_features) == sorted(lf_pds.features)
-    assert sorted(slama_linear_features.output_features) == sorted([f for f in lf_pds.features if not f.startswith('nanflg_')])
-    assert len(set(spark_ds.features).difference(slama_dataset_feats.features)) == 0
-    assert len(set(ds.features).difference(slama_dataset_feats.features)) == 0
-    assert set(slama_linear_features.output_roles.keys()) == set(f for f in lf_pds.roles.keys() if not f.startswith('nanflg_'))
-    assert all([(f in slama_dataset_feats.roles) for f in lf_pds.roles.keys() if not f.startswith('nanflg_')])
+    compare_feature_pipelines(spark, cv, ds_config, LGBAdvancedPipeline, SparkLGBAdvancedPipeline, ml_alg_kwargs)
 
-    not_equal_roles = [
-        feat
-        for feat, prole in lf_pds.roles.items()
-        if not feat.startswith('nanflg_') and
-           not (type(prole) == type(slama_linear_features.output_roles[feat]) == type(slama_dataset_feats.roles[feat]))
-    ]
-    assert len(not_equal_roles) == 0, f"Roles are different: {not_equal_roles}"
-    pass
 
+@pytest.mark.parametrize("ds_config,cv", [(ds, 3) for ds in get_test_datasets(setting="fast")])
+def test_lgbsimple_features(spark: SparkSession, ds_config: Dict[str, Any], cv: int):
+    ml_alg_kwargs = {}
+    compare_feature_pipelines(spark, cv, ds_config, LGBSimpleFeatures, SparkLGBSimpleFeatures, ml_alg_kwargs)
 
 # @pytest.mark.parametrize("ds_config,cv", [(ds, 3) for ds in get_test_datasets(setting="fast")])
 # def test_lgbadv_features(spark: SparkSession, ds_config: Dict[str, Any], cv: int):
