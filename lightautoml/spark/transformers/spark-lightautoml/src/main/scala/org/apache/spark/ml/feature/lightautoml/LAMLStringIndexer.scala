b7821c3e855db2ef4c302e2d490cc87f699942c7
Sergey Teryoshkin <se.teryoshkin@gmail.com>
2022-02-24 20:20:09 +0300

    works

--- lightautoml/spark/transformers/spark-lightautoml/src/main/scala/org/apache/spark/ml/feature/lightautoml/LAMLStringIndexer.scala
+++ lightautoml/spark/transformers/spark-lightautoml/src/main/scala/org/apache/spark/ml/feature/lightautoml/LAMLStringIndexer.scala
@@ -1,101 +1,323 @@
 package org.apache.spark.ml.feature.lightautoml
 
 import org.apache.hadoop.fs.Path
-
-import org.apache.spark.ml.feature.{StringIndexer, StringIndexerModel}
+import org.apache.spark.SparkException
+import org.apache.spark.ml.feature.{StringIndexer, StringIndexerAggregator, StringIndexerBase, StringIndexerModel}
 import org.apache.spark.annotation.Since
+import org.apache.spark.ml.Model
+import org.apache.spark.ml.attribute.NominalAttribute
+import org.apache.spark.ml.feature.StringIndexerModel.StringIndexModelWriter
+import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.ml.util._
+import org.apache.spark.sql.catalyst.expressions.{If, Literal}
+import org.apache.spark.sql.functions.{collect_set, udf}
+import org.apache.spark.sql.types.{StringType, StructType}
+import org.apache.spark.sql.{Column, DataFrame, Dataset, Encoder, Encoders}
+import org.apache.spark.util.ThreadUtils
 import org.apache.spark.util.VersionUtils.majorMinorVersion
+import org.apache.spark.util.collection.OpenHashMap
+
+@Since("1.4.0")
+class LAMLStringIndexer @Since("1.4.0")(@Since("1.4.0") override val uid: String,
+                                                minFreq: Long = 5) extends StringIndexer {
+
+  @Since("1.4.0")
+  def this() = this(Identifiable.randomUID("strIdx"))
+
+  def this(minFreq: Long) = this(Identifiable.randomUID("strIdx"), minFreq)
+
+  private def getSelectedCols(dataset: Dataset[_], inputCols: Seq[String]): Seq[Column] = {
+    inputCols.map { colName =>
+      val col = dataset.col(colName)
+      if (col.expr.dataType == StringType) {
+        col
+      } else {
+        // We don't count for NaN values. Because `StringIndexerAggregator` only processes strings,
+        // we replace NaNs with null in advance.
+        new Column(If(col.isNaN.expr, Literal(null), col.expr)).cast(StringType)
+      }
+    }
+  }
+
+  private def countByValue(dataset: Dataset[_],
+                           inputCols: Array[String]): Array[OpenHashMap[String, Long]] = {
+
+    val aggregator = new StringIndexerAggregator(inputCols.length)
+    implicit val encoder: Encoder[Array[OpenHashMap[String, Long]]] = Encoders.kryo[Array[OpenHashMap[String, Long]]]
+
+    val selectedCols = getSelectedCols(dataset, inputCols)
+    dataset.select(selectedCols: _*)
+            .toDF
+            .groupBy().agg(aggregator.toColumn)
+            .as[Array[OpenHashMap[String, Long]]]
+            .collect()(0)
+  }
+
+  private def sortByFreq(dataset: Dataset[_], ascending: Boolean): Array[Array[String]] = {
+    val (inputCols, _) = getInOutCols()
+
+    val sortFunc = StringIndexer.getSortFunc(ascending = ascending)
+    val orgStrings = countByValue(dataset, inputCols).toSeq
+    ThreadUtils.parmap(orgStrings, "sortingStringLabels", 8) { counts =>
+      counts.toSeq.filter(_._2 > minFreq).sortWith(sortFunc).map(_._1).toArray
+    }.toArray
+  }
+
+  private def sortByAlphabet(dataset: Dataset[_], ascending: Boolean): Array[Array[String]] = {
+    val (inputCols, _) = getInOutCols()
+
+    val selectedCols = getSelectedCols(dataset, inputCols).map(collect_set)
+    val allLabels = dataset.select(selectedCols: _*)
+            .collect().toSeq.flatMap(_.toSeq)
+            .asInstanceOf[scala.collection.Seq[scala.collection.Seq[String]]].toSeq
+    ThreadUtils.parmap(allLabels, "sortingStringLabels", 8) { labels =>
+      val sorted = labels.filter(_ != null).sorted
+      if (ascending) {
+        sorted.toArray
+      } else {
+        sorted.reverse.toArray
+      }
+    }.toArray
+  }
+
+  @Since("2.0.0")
+  override def fit(dataset: Dataset[_]): StringIndexerModel = {
+    transformSchema(dataset.schema, logging = true)
+
+    // In case of equal frequency when frequencyDesc/Asc, the strings are further sorted
+    // alphabetically.
+    val labelsArray = $(stringOrderType) match {
+      case StringIndexer.frequencyDesc => sortByFreq(dataset, ascending = false)
+      case StringIndexer.frequencyAsc => sortByFreq(dataset, ascending = true)
+      case StringIndexer.alphabetDesc => sortByAlphabet(dataset, ascending = false)
+      case StringIndexer.alphabetAsc => sortByAlphabet(dataset, ascending = true)
+    }
+    copyValues(
+      new LAMLStringIndexerModel(
+        uid = uid,
+        labelsArray = labelsArray,
+        defaultValue = 0.0//defaultValue
+      ).setParent(this)
+    )
+  }
+
+}
+
 
 @Since("1.6.0")
-object LAMLStringIndexer extends DefaultParamsReadable[StringIndexer] {
-    private[feature] val SKIP_INVALID: String = "skip"
-    private[feature] val ERROR_INVALID: String = "error"
-    private[feature] val KEEP_INVALID: String = "keep"
-    private[feature] val supportedHandleInvalids: Array[String] =
-        Array(SKIP_INVALID, ERROR_INVALID, KEEP_INVALID)
-    private[feature] val frequencyDesc: String = "frequencyDesc"
-    private[feature] val frequencyAsc: String =  "frequencyAsc"
-    private[feature] val alphabetDesc: String = "alphabetDesc"
-    private[feature] val alphabetAsc: String = "alphabetAsc"
-    private[feature] val supportedStringOrderType: Array[String] =
-        Array(frequencyDesc, frequencyAsc, alphabetDesc, alphabetAsc)
+object LAMLStringIndexer extends DefaultParamsReadable[LAMLStringIndexer] {
+  private[feature] val SKIP_INVALID: String = "skip"
+  private[feature] val ERROR_INVALID: String = "error"
+  private[feature] val KEEP_INVALID: String = "keep"
+  private[feature] val supportedHandleInvalids: Array[String] =
+    Array(SKIP_INVALID, ERROR_INVALID, KEEP_INVALID)
+  private[feature] val frequencyDesc: String = "frequencyDesc"
+  private[feature] val frequencyAsc: String = "frequencyAsc"
+  private[feature] val alphabetDesc: String = "alphabetDesc"
+  private[feature] val alphabetAsc: String = "alphabetAsc"
+  private[feature] val supportedStringOrderType: Array[String] =
+    Array(frequencyDesc, frequencyAsc, alphabetDesc, alphabetAsc)
 
-    @Since("1.6.0")
-    override def load(path: String): StringIndexer = super.load(path)
+  @Since("1.6.0")
+  override def load(path: String): LAMLStringIndexer = super.load(path)
 
-    // Returns a function used to sort strings by frequency (ascending or descending).
-    // In case of equal frequency, it sorts strings by alphabet (ascending).
-    private[feature] def getSortFunc(ascending: Boolean): ((String, Long), (String, Long)) => Boolean = {
-        if (ascending) {
-            case ((strA: String, freqA: Long), (strB: String, freqB: Long)) =>
-                if (freqA == freqB) {
-                    strA < strB
-                } else {
-                    freqA < freqB
-                }
+  // Returns a function used to sort strings by frequency (ascending or descending).
+  // In case of equal frequency, it sorts strings by alphabet (ascending).
+  private[feature] def getSortFunc(ascending: Boolean): ((String, Long), (String, Long)) => Boolean = {
+    if (ascending) {
+      case ((strA: String, freqA: Long), (strB: String, freqB: Long)) =>
+        if (freqA == freqB) {
+          strA < strB
         } else {
-            case ((strA: String, freqA: Long), (strB: String, freqB: Long)) =>
-                if (freqA == freqB) {
-                    strA < strB
-                } else {
-                    freqA > freqB
-                }
+          freqA < freqB
+        }
+    } else {
+      case ((strA: String, freqA: Long), (strB: String, freqB: Long)) =>
+        if (freqA == freqB) {
+          strA < strB
+        } else {
+          freqA > freqB
         }
     }
+  }
 }
 
 
-@Since("1.6.0")
-object LAMLStringIndexerModel extends MLReadable[StringIndexerModel] {
+@Since("1.4.0")
+class LAMLStringIndexerModel(override val uid: String,
+                             override val labelsArray: Array[Array[String]],
+                             defaultValue: Double = 0.0)
+        extends StringIndexerModel(labelsArray) {
 
-    private[LAMLStringIndexerModel]
-    class StringIndexModelWriter(instance: StringIndexerModel) extends MLWriter {
+//  import LAMLStringIndexerModel._
 
-        private case class Data(labelsArray: Array[Array[String]])
+//  def this(uid: String, labels: Array[String]) = this(uid, Array(labels))
+//
+//  def this(labels: Array[String]) = this(Identifiable.randomUID("strIdx"), Array(labels))
+//
+//  def this(labelsArray: Array[Array[String]]) = this(Identifiable.randomUID("strIdx"), labelsArray)
 
-        override protected def saveImpl(path: String): Unit = {
-            DefaultParamsWriter.saveMetadata(instance, path, sc)
-            val data = Data(instance.labelsArray)
-            val dataPath = new Path(path, "data").toString
-            sparkSession.createDataFrame(Seq(data)).repartition(1).write.parquet(dataPath)
-        }
+  // Prepares the maps for string values to corresponding index values.
+  private val labelsToIndexArray: Array[OpenHashMap[String, Double]] = {
+    for (labels <- labelsArray) yield {
+      val n = labels.length
+      val map = new OpenHashMap[String, Double](n)
+      labels.zipWithIndex.foreach { case (label, idx) =>
+        map.update(label, idx + 1)
+      }
+      map
+    }
+  }
+
+  // This filters out any null values and also the input labels which are not in
+  // the dataset used for fitting.
+  private def filterInvalidData(dataset: Dataset[_], inputColNames: Seq[String]): Dataset[_] = {
+    val conditions: Seq[Column] = (0 until inputColNames.length).map { i =>
+      val inputColName = inputColNames(i)
+      val labelToIndex = labelsToIndexArray(i)
+      // We have this additional lookup at `labelToIndex` when `handleInvalid` is set to
+      // `StringIndexer.SKIP_INVALID`. Another idea is to do this lookup natively by SQL
+      // expression, however, lookup for a key in a map is not efficient in SparkSQL now.
+      // See `ElementAt` and `GetMapValue` expressions. If SQL's map lookup is improved,
+      // we can consider to change this.
+      val filter = udf { label: String =>
+        labelToIndex.contains(label)
+      }
+      filter(dataset(inputColName))
     }
 
-    private class StringIndexerModelReader extends MLReader[StringIndexerModel] {
+    dataset.na.drop(inputColNames.filter(dataset.schema.fieldNames.contains(_)))
+            .where(conditions.reduce(_ and _))
+  }
 
-        private val className = classOf[StringIndexerModel].getName
+  private def getIndexer(labels: Seq[String], labelToIndex: OpenHashMap[String, Double]) = {
+    val keepInvalid = (getHandleInvalid == StringIndexer.KEEP_INVALID)
 
-        override def load(path: String): StringIndexerModel = {
-            val metadata = DefaultParamsReader.loadMetadata(path, sc, className)
-            val dataPath = new Path(path, "data").toString
-
-            // We support loading old `StringIndexerModel` saved by previous Spark versions.
-            // Previous model has `labels`, but new model has `labelsArray`.
-            val (majorVersion, minorVersion) = majorMinorVersion(metadata.sparkVersion)
-            val labelsArray = if (majorVersion < 3) {
-                // Spark 2.4 and before.
-                val data = sparkSession.read.parquet(dataPath)
-                        .select("labels")
-                        .head()
-                val labels = data.getAs[Seq[String]](0).toArray
-                Array(labels)
-            } else {
-                // After Spark 3.0.
-                val data = sparkSession.read.parquet(dataPath)
-                        .select("labelsArray")
-                        .head()
-                data.getSeq[scala.collection.Seq[String]](0).map(_.toArray).toArray
-            }
-            val model = new StringIndexerModel(metadata.uid, labelsArray)
-            metadata.getAndSetParams(model)
-            model
+    udf { label: String =>
+      if (label == null) {
+        if (keepInvalid) {
+          labels.length
+        } else {
+          throw new SparkException("StringIndexer encountered NULL value. To handle or skip " +
+                  "NULLS, try setting StringIndexer.handleInvalid.")
         }
+      } else {
+        if (labelToIndex.contains(label)) {
+          labelToIndex(label)
+        } else if (keepInvalid) {
+          defaultValue
+        } else {
+          throw new SparkException(s"Unseen label: $label. To handle unseen labels, " +
+                  s"set Param handleInvalid to ${StringIndexer.KEEP_INVALID}.")
+        }
+      }
+    }.asNondeterministic()
+  }
+
+  @Since("2.0.0")
+  override def transform(dataset: Dataset[_]): DataFrame = {
+    transformSchema(dataset.schema, logging = true)
+
+    val (inputColNames, outputColNames) = getInOutCols()
+    val outputColumns = new Array[Column](outputColNames.length)
+
+    // Skips invalid rows if `handleInvalid` is set to `StringIndexer.SKIP_INVALID`.
+    val filteredDataset = if (getHandleInvalid == StringIndexer.SKIP_INVALID) {
+      filterInvalidData(dataset, inputColNames)
+    } else {
+      dataset
     }
 
-    @Since("1.6.0")
-    override def read: MLReader[StringIndexerModel] = new StringIndexerModelReader
+    for (i <- 0 until outputColNames.length) {
+      val inputColName = inputColNames(i)
+      val outputColName = outputColNames(i)
+      val labelToIndex = labelsToIndexArray(i)
+      val labels = labelsArray(i)
 
-    @Since("1.6.0")
-    override def load(path: String): StringIndexerModel = super.load(path)
+      if (!dataset.schema.fieldNames.contains(inputColName)) {
+        logWarning(s"Input column ${inputColName} does not exist during transformation. " +
+                "Skip StringIndexerModel for this column.")
+        outputColNames(i) = null
+      } else {
+        val filteredLabels = getHandleInvalid match {
+          case StringIndexer.KEEP_INVALID => labels :+ "__unknown"
+          case _ => labels
+        }
+        val metadata = NominalAttribute.defaultAttr
+                .withName(outputColName)
+                .withValues(filteredLabels)
+                .toMetadata()
+
+        val indexer = getIndexer(labels, labelToIndex)
+
+        outputColumns(i) = indexer(dataset(inputColName).cast(StringType))
+                .as(outputColName, metadata)
+      }
+    }
+
+    val filteredOutputColNames = outputColNames.filter(_ != null)
+    val filteredOutputColumns = outputColumns.filter(_ != null)
+
+    require(filteredOutputColNames.length == filteredOutputColumns.length)
+    if (filteredOutputColNames.length > 0) {
+      filteredDataset.withColumns(filteredOutputColNames, filteredOutputColumns)
+    } else {
+      filteredDataset.toDF()
+    }
+  }
 }
+
+
+//@Since("1.6.0")
+//object LAMLStringIndexerModel extends MLReadable[StringIndexerModel] {
+//
+//  private[LAMLStringIndexerModel]
+//  class StringIndexModelWriter(instance: LAMLStringIndexerModel) extends MLWriter {
+//
+//    private case class Data(labelsArray: Array[Array[String]])
+//
+//    override protected def saveImpl(path: String): Unit = {
+//      DefaultParamsWriter.saveMetadata(instance, path, sc)
+//      val data = Data(instance.labelsArray)
+//      val dataPath = new Path(path, "data").toString
+//      sparkSession.createDataFrame(Seq(data)).repartition(1).write.parquet(dataPath)
+//    }
+//  }
+//
+//  private class StringIndexerModelReader extends MLReader[StringIndexerModel] {
+//
+//    private val className = classOf[StringIndexerModel].getName
+//
+//    override def load(path: String): StringIndexerModel = {
+//      val metadata = DefaultParamsReader.loadMetadata(path, sc, className)
+//      val dataPath = new Path(path, "data").toString
+//
+//      // We support loading old `StringIndexerModel` saved by previous Spark versions.
+//      // Previous model has `labels`, but new model has `labelsArray`.
+//      val (majorVersion, minorVersion) = majorMinorVersion(metadata.sparkVersion)
+//      val labelsArray = if (majorVersion < 3) {
+//        // Spark 2.4 and before.
+//        val data = sparkSession.read.parquet(dataPath)
+//                .select("labels")
+//                .head()
+//        val labels = data.getAs[Seq[String]](0).toArray
+//        Array(labels)
+//      } else {
+//        // After Spark 3.0.
+//        val data = sparkSession.read.parquet(dataPath)
+//                .select("labelsArray")
+//                .head()
+//        data.getSeq[scala.collection.Seq[String]](0).map(_.toArray).toArray
+//      }
+//      val model = new StringIndexerModel(metadata.uid, labelsArray)
+//      metadata.getAndSetParams(model)
+//      model
+//    }
+//  }
+//
+//  @Since("1.6.0")
+//  override def read: MLReader[StringIndexerModel] = new StringIndexerModelReader
+//
+//  @Since("1.6.0")
+//  override def load(path: String): StringIndexerModel = super.load(path)
+//}
--- lightautoml/spark/transformers/spark-lightautoml/src/test/scala/org/apache/spark/ml/feature/lightautoml/TestLAMLStringIndexer.scala
+++ lightautoml/spark/transformers/spark-lightautoml/src/test/scala/org/apache/spark/ml/feature/lightautoml/TestLAMLStringIndexer.scala
@@ -1,5 +1,61 @@
 package org.apache.spark.ml.feature.lightautoml
 
-class TestLAMLStringIndexer {
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.lightautoml.LAMLStringIndexer
+
+
+object TestLAMLStringIndexer extends App {
+
+  val file = "file:///D:\\Projects\\Sber\\LAMA\\Sber-LAMA-Stuff\\stringindexer-data\\data.json"
+  val testFile = "file:///D:\\Projects\\Sber\\LAMA\\Sber-LAMA-Stuff\\stringindexer-data\\test_data.json"
+  val spark = SparkSession
+          .builder()
+          .appName("test")
+          .master("local[1]")
+          .getOrCreate()
+
+  //import spark.sqlContext.implicits._
+
+  val df = spark.read.json(file).cache()
+  val testDf = spark.read.json(testFile).cache()
+  val cnt = df.count()
+
+  println("-- Source --")
+  df.show(100)
+
+  val startTime = System.currentTimeMillis()
+
+  val indexer = new StringIndexer(uid = "abc").setInputCol("value").setOutputCol("index").setHandleInvalid("keep")
+  println(indexer.uid)
+
+  val model = indexer.fit(df)
+  val testIndexed = model.transform(testDf)
+
+  println("-- Spark Indexed --")
+  testIndexed.show(100)
+
+  val lamaIndexer = new LAMLStringIndexer(uid = "abc", minFreq = 5)
+          .setInputCol("value")
+          .setOutputCol("index")
+          .setHandleInvalid("keep")
+
+  println(lamaIndexer.uid)
+
+  val lamaModel = lamaIndexer.fit(df)
+  val lamaTestIndexed = lamaModel.transform(testDf)
+
+  println("-- Lama Indexed --")
+  lamaTestIndexed.show(100)
+
+  val endTime = System.currentTimeMillis()
+
+  println(s"Duration = ${(endTime - startTime) / 1000D} seconds")
+  println(s"Size: ${cnt}")
+
+  while (args(0).toBoolean) {
+    Thread.sleep(1000)
+  }
+
 
 }
