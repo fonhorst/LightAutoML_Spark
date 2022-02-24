fixed averaging blender

lightautoml/spark/automl/blend.py
@@ -181,20 +181,12 @@ class SparkBestModelSelector(SparkBlender, WeightedBlender):
 
 class SparkWeightedBlender(SparkBlender, WeightedBlender):
     def __init__(self, max_iters: int = 5, max_inner_iters: int = 7, max_nonzero_coef: float = 0.05,):
-        super().__init__()
-        super(WeightedBlender, self).__init__(max_iters, max_inner_iters, max_nonzero_coef)
+        SparkBlender.__init__(self)
+        WeightedBlender.__init__(self, max_iters, max_inner_iters, max_nonzero_coef)
         self._predictions_dataset: Optional[SparkDataset] = None
 
     def _get_weighted_pred(self, splitted_preds: Sequence[str], wts: Optional[np.ndarray]) -> SparkDataset:
-        avr = AveragingTransformer(
-            task_name=self._task.name,
-            input_cols=list(splitted_preds),
-            output_col=self._single_prediction_col_name,
-            remove_cols=list(splitted_preds),
-            convert_to_array_first=True,
-            weights=wts,
-            dim_num=self._outp_dim
-        )
+        avr = self._build_avr_transformer(splitted_preds, wts)
 
         weighted_preds_sdf = avr.transform(self._predictions_dataset.data)
 
@@ -203,6 +195,17 @@ class SparkWeightedBlender(SparkBlender, WeightedBlender):
 
         return wpreds_sds
 
+    def _build_avr_transformer(self, splitted_preds: Sequence[str], wts: Optional[np.ndarray]) -> AveragingTransformer:
+        return AveragingTransformer(
+            task_name=self._task.name,
+            input_cols=list(splitted_preds),
+            output_col=self._single_prediction_col_name,
+            remove_cols=list(splitted_preds),
+            convert_to_array_first=True,
+            weights=wts.tolist(),
+            dim_num=self._outp_dim
+        )
+
     def _fit_predict(self, predictions: SparkDataset, pipes: Sequence[SparkMLPipeline]) \
             -> Tuple[SparkDataset, Sequence[SparkMLPipeline]]:
 
@@ -210,7 +213,7 @@ class SparkWeightedBlender(SparkBlender, WeightedBlender):
 
         sm = self.split_models(predictions, pipes)
         pred_cols = [pred_col for pred_col, _, _ in sm]
-        pipe_idx = np.ndarray([pidx for _, _, pidx in sm])
+        pipe_idx = np.array([pidx for _, _, pidx in sm])
 
         wts = self._optimize(pred_cols)
 
@@ -218,6 +221,7 @@ class SparkWeightedBlender(SparkBlender, WeightedBlender):
         pipes, self.wts = self._prune_pipe(pipes, wts, pipe_idx)
         pipes = cast(Sequence[SparkMLPipeline], pipes)
 
+        self._transformer = self._build_avr_transformer(reweighted_pred_cols, self.wts)
         outp = self._get_weighted_pred(reweighted_pred_cols, self.wts)
 
         return outp, pipes

tests/spark/unit/test_blenders/test_blenders.py
@@ -9,6 +9,7 @@ from pyspark.sql import SparkSession
 
 from lightautoml.automl.blend import BestModelSelector, Blender, WeightedBlender
 from lightautoml.dataset.np_pd_dataset import NumpyDataset
+from lightautoml.dataset.roles import NumericRole
 from lightautoml.spark.automl.blend import SparkBestModelSelector, SparkWeightedBlender
 
 from lightautoml.spark.dataset.base import SparkDataset
@@ -18,6 +19,8 @@ from lightautoml.spark.ml_algo.linear_pyspark import SparkLinearLBFGS
 from lightautoml.spark.pipelines.ml.base import SparkMLPipeline
 from lightautoml.spark.pipelines.ml.nested_ml_pipe import SparkNestedTabularMLPipeline as SparkNestedTabularMLPipeline
 from lightautoml.spark.tasks.base import SparkTask as SparkTask
+from lightautoml.spark.transformers.base import ColumnsSelectorTransformer, DropColumnsTransformer
+from lightautoml.spark.validation.iterators import SparkDummyIterator
 from lightautoml.tasks import Task
 from .. import from_pandas_to_spark, spark as spark_sess, compare_obtained_datasets
 
@@ -79,36 +82,41 @@ def do_compare_blenders(spark: SparkSession, lama_blender: Blender, spark_blende
 
 
 def test_weighted_blender(spark: SparkSession):
-    target_col = "target"
+    target_col = "some_target"
+    folds_col = "folds"
     n_classes = 10
     models_count = 4
 
     data = [
-        {f"pred_{i}": DenseVector([j for j in range(n_classes)]) for i in range(models_count)}
-        for _ in range(100)
+        {
+            SparkDataset.ID_COLUMN: i,
+            "a": i, "b": 100 + i, "c": 100 * i,
+            target_col: random.randint(0, n_classes),
+            folds_col: random.randint(0, 2)
+        }
+        for i in range(100)
     ]
-    for i, d in enumerate(data):
-        d[SparkDataset.ID_COLUMN] = i
-        d[target_col] = random.randint(0, 5)
 
-    roles = {
-        f"pred_{i}": NumericVectorOrArrayRole(
-            size=n_classes,
-            element_col_name_template=f"pred_{i}" + "_{}",
-            dtype=np.float32,
-            force_input=True,
-            prob=False
-        ) for i in range(models_count)
-    }
+    roles = {"a": NumericRole(), "b": NumericRole(), "c": NumericRole()}
+
+    data_sdf = spark.createDataFrame(data)
+    data_sds = SparkDataset(data=data_sdf, task=SparkTask("multiclass"),
+                            roles=roles, target=target_col, folds=folds_col)
 
     pipes = [
         SparkMLPipeline(cacher_key='test_cache', ml_algos=[DummyMLAlgo(n_classes, name=f"dummy_0_{i}")])
         for i in range(models_count)
     ]
 
-    predictions_sdf = spark.createDataFrame(data)
-    pred_sds = SparkDataset(data=predictions_sdf, task=SparkTask("multiclass"), roles=roles, target=target_col)
+    for pipe in pipes:
+        data_sds = pipe.fit_predict(SparkDummyIterator(data_sds))
+
+    preds_roles = {c: role for c, role in data_sds.roles.items() if c not in roles}
+
+    sdf = data_sds.data.drop(*list(roles.keys()))
+    ml_ds = data_sds.empty()
+    ml_ds.set_data(sdf, list(preds_roles.keys()), preds_roles)
 
     swb = SparkWeightedBlender()
-    blended_sds, filtered_pipes = swb.fit_predict(pred_sds, pipes)
-    transformed_preds_sdf = swb.transformer.transform(pred_sds.data)
+    blended_sds, filtered_pipes = swb.fit_predict(ml_ds, pipes)
+    transformed_preds_sdf = swb.transformer.transform(ml_ds.data)