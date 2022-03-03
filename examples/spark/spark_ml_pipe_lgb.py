c313d2e571cc524c4900d796882f7433800daba6
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-03-03 12:58:29 +0300

    lgb pipe example

--- examples/spark/spark_ml_pipe_lgb.py
+++ examples/spark/spark_ml_pipe_lgb.py
@@ -1,9 +1,9 @@
 import logging.config
 import logging.config
-from copy import deepcopy
+
+from pyspark.sql import SparkSession
 
 from lightautoml.pipelines.selection.importance_based import ImportanceCutoffSelector, ModelBasedImportanceEstimator
-from lightautoml.spark.dataset.base import SparkDataset
 from lightautoml.spark.ml_algo.boost_lgbm import SparkBoostLGBM
 from lightautoml.spark.pipelines.features.lgb_pipeline import SparkLGBAdvancedPipeline, SparkLGBSimpleFeatures
 from lightautoml.spark.pipelines.ml.base import SparkMLPipeline
@@ -17,56 +17,60 @@ logging.basicConfig(level=logging.INFO, format=VERBOSE_LOGGING_FORMAT)
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    with spark_session(master="local[4]") as spark:
-        roles = {
-            "target": 'price',
-            "drop": ["dealer_zip", "description", "listed_date",
-                     "year", 'Unnamed: 0', '_c0',
-                     'sp_id', 'sp_name', 'trimId',
-                     'trim_name', 'major_options', 'main_picture_url',
-                     'interior_color', 'exterior_color'],
-            "numeric": ['latitude', 'longitude', 'mileage']
-        }
+    spark = SparkSession.builder.getOrCreate()
 
-        with log_exec_time():
-            # data reading and converting to SparkDataset
-            df = spark.read.csv("examples/data/tiny_used_cars_data.csv", header=True, escape="\"")
-            task = SparkTask("reg")
-            sreader = SparkToSparkReader(task=task, cv=3, advanced_roles=False)
-            sdataset = sreader.fit_read(df, roles=roles)
+    seed = 100
+    path = "/opt/spark_data/sampled_app_train.csv"
+    task_type = "binary"
+    roles = {"target": "TARGET", "drop": ["SK_ID_CURR"]}
+    ml_alg_kwargs = {
+        'auto_unique_co': 10,
+        'max_intersection_depth': 3,
+        'multiclass_te_co': 3,
+        'output_categories': True,
+        'top_intersections': 4
+    }
+    cacher_key = "main_cache"
 
-            ml_alg_kwargs = {
-                'auto_unique_co': 10,
-                'max_intersection_depth': 3,
-                'multiclass_te_co': 3,
-                'output_categories': True,
-                'top_intersections': 4
-            }
+    with log_exec_time():
 
-            cacher_key = "main_cache"
+        df = spark.read.csv(path, header=True, escape="\"")
+        train_df, test_df = df.randomSplit([0.8, 0.2], seed)
 
-            iterator = SparkFoldsIterator(sdataset, n_folds=3)
+        task = SparkTask(task_type)
+        score = task.get_dataset_metric()
 
-            spark_ml_algo = SparkBoostLGBM(cacher_key='example', freeze_defaults=False)
-            spark_features_pipeline = SparkLGBAdvancedPipeline(cacher_key=cacher_key, **ml_alg_kwargs)
-            spark_selector = ImportanceCutoffSelector(
-                cutoff=0.0,
-                feature_pipeline=SparkLGBSimpleFeatures(cacher_key='preselector'),
-                ml_algo=SparkBoostLGBM(cacher_key='example', freeze_defaults=False),
-                imp_estimator=ModelBasedImportanceEstimator()
-            )
+        sreader = SparkToSparkReader(task=task, cv=3, advanced_roles=False)
+        sdataset = sreader.fit_read(df, roles=roles)
 
-            ml_pipe = SparkMLPipeline(
-                cacher_key=cacher_key,
-                ml_algos=[spark_ml_algo],
-                pre_selection=spark_selector,
-                features_pipeline=spark_features_pipeline,
-                post_selection=None
-            )
+        iterator = SparkFoldsIterator(sdataset, n_folds=3)
 
-            _ = ml_pipe.fit_predict(iterator)
+        spark_ml_algo = SparkBoostLGBM(cacher_key=cacher_key, freeze_defaults=False)
+        spark_features_pipeline = SparkLGBAdvancedPipeline(cacher_key=cacher_key, **ml_alg_kwargs)
+        spark_selector = ImportanceCutoffSelector(
+            cutoff=0.0,
+            feature_pipeline=SparkLGBSimpleFeatures(cacher_key='preselector'),
+            ml_algo=SparkBoostLGBM(cacher_key=cacher_key, freeze_defaults=False),
+            imp_estimator=ModelBasedImportanceEstimator()
+        )
 
-            final_result = ml_pipe.transformer.transform(sdataset.data)
-            final_result.write.mode('overwrite').format('noop').save()
+        ml_pipe = SparkMLPipeline(
+            cacher_key=cacher_key,
+            ml_algos=[spark_ml_algo],
+            pre_selection=spark_selector,
+            features_pipeline=spark_features_pipeline,
+            post_selection=None
+        )
 
-        logger.info("Finished")
+        oof_preds_ds = ml_pipe.fit_predict(iterator)
+        oof_score = score(oof_preds_ds)
+        logger.info(f"OOF score: {oof_score}")
+
+        test_sds = sreader.read(test_df, add_array_attrs=True)
+        test_preds_ds = ml_pipe.predict(test_sds)
+        test_score = score(test_preds_ds)
+        logger.info(f"Test score: {oof_score}")
+
+    logger.info("Finished")
+
+    spark.stop()
--- examples/spark/tabular_preset_automl.py
+++ examples/spark/tabular_preset_automl.py
@@ -1,5 +1,4 @@
 import logging.config
-import socket
 from typing import Tuple
 
 import pyspark.sql.functions as F
@@ -8,7 +7,7 @@ from pyspark.sql import SparkSession
 from lightautoml.spark.automl.presets.tabular_presets import SparkTabularAutoML
 from lightautoml.spark.dataset.base import SparkDataFrame, SparkDataset
 from lightautoml.spark.tasks.base import SparkTask
-from lightautoml.spark.utils import log_exec_timer, spark_session, logging_config, VERBOSE_LOGGING_FORMAT
+from lightautoml.spark.utils import log_exec_timer, logging_config, VERBOSE_LOGGING_FORMAT
 
 logging.config.dictConfig(logging_config(level=logging.INFO, log_filename='/tmp/lama.log'))
 logging.basicConfig(level=logging.DEBUG, format=VERBOSE_LOGGING_FORMAT)
@@ -24,7 +23,6 @@ def prepare_test_and_train(spark: SparkSession, path:str, seed: int) -> Tuple[Sp
         F.rand(seed).alias('is_test')
     ).cache()
     data.write.mode('overwrite').format('noop').save()
-    # train_data, test_data = data.randomSplit([0.8, 0.2], seed=seed)
 
     train_data = data.where(F.col('is_test') < 0.8).drop('is_test').cache()
     test_data = data.where(F.col('is_test') >= 0.8).drop('is_test').cache()
@@ -35,116 +33,67 @@ def prepare_test_and_train(spark: SparkSession, path:str, seed: int) -> Tuple[Sp
     return train_data, test_data
 
 
-local_ip_address = socket.gethostbyname(socket.gethostname())
+if __name__ == "__main__":
+    spark = SparkSession.builder.getOrCreate()
 
+    seed = 42
+    cv = 5
+    use_algos = [["lgb", "linear_l2"], ["lgb"]]
+    path = "/opt/spark_data/small_used_cars_data.csv"
+    task_type = "reg"
+    roles = {
+                    "target": "price",
+                    "drop": ["dealer_zip", "description", "listed_date",
+                             "year", 'Unnamed: 0', '_c0',
+                             'sp_id', 'sp_name', 'trimId',
+                             'trim_name', 'major_options', 'main_picture_url',
+                             'interior_color', 'exterior_color'],
+                    "numeric": ['latitude', 'longitude', 'mileage']
+                }
 
-spark = (
-    SparkSession
-    .builder
-    .master("k8s://https://10.32.15.3:6443")
-    .config("spark.driver.bindAddress", '0.0.0.0')
-    .config('spark.driver.host', local_ip_address)
-    # .config('spark.kubernetes.container.image', 'node2.bdcl:5000/spark-py-lama:lama-v3.2.0')
-    .config('spark.kubernetes.container.image', 'node2.bdcl:5000/spark-lama-k8s:3.9-3.2.0')
-    .config('spark.kubernetes.namespace', 'spark-lama-exps')
-    .config('spark.kubernetes.authenticate.driver.serviceAccountName', 'spark')
-    .config('spark.kubernetes.memoryOverheadFactor', '0.4')
-    .config('spark.kubernetes.driver.label.appname', 'driver-test-submit-run')
-    .config('spark.kubernetes.executor.label.appname', 'executor-test-submit-run')
-    .config('spark.kubernetes.executor.deleteOnTermination', 'false')
-    .config('spark.scheduler.minRegisteredResourcesRatio', '1.0')
-    .config('spark.scheduler.maxRegisteredResourcesWaitingTime', '180s')
-    .config('spark.jars', 'jars/spark-lightautoml_2.12-0.1.jar')
-    .config('spark.jars.packages', 'com.microsoft.azure:synapseml_2.12:0.9.5')
-    .config('spark.jars.repositories', 'https://mmlspark.azureedge.net/maven')
-    .config('spark.driver.cores', '4')
-    .config('spark.driver.memory', '32g')
-    .config('spark.executor.instances', '4')
-    .config('spark.executor.cores', '4')
-    .config('spark.executor.memory', '16g')
-    .config('spark.cores.max', '16')
-    .config('spark.memory.fraction', '0.6')
-    .config('spark.memory.storageFraction', '0.5')
-    .config('spark.sql.autoBroadcastJoinThreshold', '100MB')
-    .config('spark.sql.execution.arrow.pyspark.enabled', 'true')
-    .config('spark.kubernetes.file.upload.path', '/mnt/nfs/spark_upload_dir')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.scripts-shared-vol.options.claimName', 'scripts-shared-vol')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.scripts-shared-vol.options.storageClass', 'local-hdd')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.scripts-shared-vol.mount.path', '/scripts/')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.scripts-shared-vol.mount.readOnly', 'true')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.spark-lama-data.options.claimName', 'spark-lama-data')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.spark-lama-data.options.storageClass', 'local-hdd')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.spark-lama-data.mount.path', '/opt/spark_data/')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.spark-lama-data.mount.readOnly', 'true')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.spark-lama-data.options.claimName', 'spark-lama-data')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.spark-lama-data.options.storageClass', 'local-hdd')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.spark-lama-data.mount.path', '/opt/spark_data/')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.spark-lama-data.mount.readOnly', 'true')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.exp-results-vol.options.claimName', 'exp-results-vol')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.exp-results-vol.options.storageClass', 'local-hdd')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.exp-results-vol.mount.path', '/exp_results')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.exp-results-vol.mount.readOnly', 'false')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.mnt-nfs.options.claimName', 'mnt-nfs')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.mnt-nfs.options.storageClass', 'nfs')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.mnt-nfs.mount.path', '/mnt/nfs/')
-    .config('spark.kubernetes.driver.volumes.persistentVolumeClaim.mnt-nfs.mount.readOnly', 'false')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.mnt-nfs.options.claimName', 'mnt-nfs')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.mnt-nfs.options.storageClass', 'nfs')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.mnt-nfs.mount.path', '/mnt/nfs/')
-    .config('spark.kubernetes.executor.volumes.persistentVolumeClaim.mnt-nfs.mount.readOnly', 'false')
-    .getOrCreate()
-)
+    with log_exec_timer("spark-lama training") as train_timer:
+        task = SparkTask(task_type)
+        train_data, test_data = prepare_test_and_train(spark, path, seed)
 
-seed = 42
-cv = 5
-use_algos = [["lgb"]]
-path = "/opt/spark_data/sampled_app_train.csv"
-task_type = "binary"
-roles = {"target": "TARGET", "drop": ["SK_ID_CURR"]}
+        test_data_dropped = test_data
 
-with log_exec_timer("spark-lama training") as train_timer:
-    task = SparkTask(task_type)
-    train_data, test_data = prepare_test_and_train(spark, path, seed)
+        automl = SparkTabularAutoML(
+            spark=spark,
+            task=task,
+            general_params={"use_algos": use_algos},
+            reader_params={"cv": cv, "advanced_roles": False},
+            tuning_params={'fit_on_holdout': True, 'max_tuning_iter': 101, 'max_tuning_time': 3600}
+        )
 
-    test_data_dropped = test_data
+        oof_predictions = automl.fit_predict(
+            train_data,
+            roles=roles
+        )
 
-    automl = SparkTabularAutoML(
-        spark=spark,
-        task=task,
-        general_params={"use_algos": use_algos},
-        reader_params={"cv": cv, "advanced_roles": False},
-        tuning_params={'fit_on_holdout': True, 'max_tuning_iter': 101, 'max_tuning_time': 3600}
-    )
-
-    oof_predictions = automl.fit_predict(
-        train_data,
-        roles=roles
-    )
-
-logger.info("Predicting on out of fold")
-
-score = task.get_dataset_metric()
-metric_value = score(oof_predictions)
-
-logger.info(f"score for out-of-fold predictions: {metric_value}")
-
-with log_exec_timer("spark-lama predicting on test") as predict_timer:
-    te_pred = automl.predict(test_data_dropped, add_reader_attrs=True)
+    logger.info("Predicting on out of fold")
 
     score = task.get_dataset_metric()
-    test_metric_value = score(te_pred)
+    metric_value = score(oof_predictions)
 
-    logger.info(f"score for test predictions: {test_metric_value}")
+    logger.info(f"score for out-of-fold predictions: {metric_value}")
 
-logger.info("Predicting is finished")
+    with log_exec_timer("spark-lama predicting on test") as predict_timer:
+        te_pred = automl.predict(test_data_dropped, add_reader_attrs=True)
 
-result = {
-    "metric_value": metric_value,
-    "test_metric_value": test_metric_value,
-    "train_duration_secs": train_timer.duration,
-    "predict_duration_secs": predict_timer.duration
-}
+        score = task.get_dataset_metric()
+        test_metric_value = score(te_pred)
 
-print(f"EXP-RESULT: {result}")
+        logger.info(f"score for test predictions: {test_metric_value}")
 
-spark.stop()
+    logger.info("Predicting is finished")
+
+    result = {
+        "metric_value": metric_value,
+        "test_metric_value": test_metric_value,
+        "train_duration_secs": train_timer.duration,
+        "predict_duration_secs": predict_timer.duration
+    }
+
+    print(f"EXP-RESULT: {result}")
+
+    spark.stop()
