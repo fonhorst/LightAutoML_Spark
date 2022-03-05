66d59e9d27752071a382ce1566e40d354cdf0e90
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-03-05 18:57:03 +0300

    more complete version of SparkTorchBasedLinearEstimator

--- lightautoml/spark/ml_algo/torch_based/linear_model.py
+++ lightautoml/spark/ml_algo/torch_based/linear_model.py
@@ -1,53 +1,63 @@
+import logging
 import sys
-from typing import Sequence, Optional, Callable
+from copy import deepcopy
+from typing import Sequence, Optional, Callable, List, Dict
 
 import torch
 from horovod.spark.common.backend import SparkBackend
 from horovod.spark.common.store import Store
+from pyspark.ml import PipelineModel, Transformer
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.param.shared import HasPredictionCol
 from torch import optim
 
+from lightautoml.dataset.roles import CategoryRole, NumericRole, ColumnRole
 from lightautoml.ml_algo.torch_based.linear_model import ArrayOrSparseMatrix
 from lightautoml.spark.dataset.base import SparkDataset, SparkDataFrame
 
 import horovod.spark.torch as hvd
+import numpy as np
+
+from lightautoml.spark.transformers.base import SparkBaseEstimator
+import pyspark.sql.functions as F
 
 
-class SparkTorchBasedLinearEstimator:
-    """Linear model based on torch L-BFGS solver.
+logger = logging.getLogger(__name__)
 
-    Accepts Numeric + Label Encoded categories or Numeric sparse input.
-    """
 
-    def __init__(
-        self,
-        data_size: int,
-        categorical_idx: Sequence[int] = (),
-        embed_sizes: Sequence[int] = (),
-        output_size: int = 1,
-        cs: Sequence[float] = (
-            0.00001,
-            0.00005,
-            0.0001,
-            0.0005,
-            0.001,
-            0.005,
-            0.01,
-            0.05,
-            0.1,
-            0.5,
-            1.0,
-            2.0,
-            5.0,
-            7.0,
-            10.0,
-            20.0,
-        ),
-        max_iter: int = 1000,
-        tol: float = 1e-5,
-        early_stopping: int = 2,
-        loss=Optional[Callable],
-        metric=Optional[Callable],
-    ):
+class SparkTorchBasedLinearEstimator(SparkBaseEstimator, HasPredictionCol):
+    def __init__(self,
+                 input_roles: Dict[str, ColumnRole],
+                 label_col: str,
+                 prediction_col: str,
+                 prediction_role: ColumnRole,
+                 val_col: Optional[str] = None,
+                 categorical_idx: Sequence[int] = (),
+                 embed_sizes: Sequence[int] = (),
+                 output_size: int = 1,
+                 cs: Sequence[float] = (
+                    0.00001,
+                    0.00005,
+                    0.0001,
+                    0.0005,
+                    0.001,
+                    0.005,
+                    0.01,
+                    0.05,
+                    0.1,
+                    0.5,
+                    1.0,
+                    2.0,
+                    5.0,
+                    7.0,
+                    10.0,
+                    20.0,
+                 ),
+                 max_iter: int = 1000,
+                 tol: float = 1e-5,
+                 early_stopping: int = 2,
+                 loss=Optional[Callable],
+                 metric=Optional[Callable]):
         """
         Args:
             data_size: Not used.
@@ -62,7 +72,11 @@ class SparkTorchBasedLinearEstimator:
             metric: Metric function. Format: metric(y_true, y_preds, sample_weight = None) -> float (greater_is_better).
 
         """
-        self.data_size = data_size
+        super().__init__(list(input_roles.keys()), input_roles, do_replace_columns=False, output_role=prediction_role)
+        self.label_col = label_col
+        self.val_col = val_col
+        self.set(self.predictionCol, prediction_col)
+
         self.categorical_idx = categorical_idx
         self.embed_sizes = embed_sizes
         self.output_size = output_size
@@ -76,38 +90,71 @@ class SparkTorchBasedLinearEstimator:
         self.loss = loss  # loss(preds, true) -> loss_arr, assume reduction='none'
         self.metric = metric  # metric(y_true, y_preds, sample_weight = None) -> float (greater_is_better)
 
-    def _prepare_data_dense(self, data: np.ndarray):
-        """Prepare dense matrix.
+        self._transformer: Optional[PipelineModel] = None
+        self._weights_col: Optional[str] = None
 
-        Split categorical and numeric features.
+    """Linear model based on torch L-BFGS solver.
+
+    Accepts Numeric + Label Encoded categories or Numeric sparse input.
+    """
+
+    def _fit(self, data: SparkDataFrame) -> Transformer:
+        """Fit method.
 
         Args:
-            data: data to prepare.
+            data: Data to train.
+            y: Train target values.
+            weights: Train items weights.
+            data_val: Data to validate.
+            y_val: Valid target values.
+            weights_val: Validation item weights.
 
         Returns:
-            Tuple (numeric_features, cat_features).
+            self.
 
         """
-        if 0 < len(self.categorical_idx) < data.shape[1]:
-            data_cat = torch.from_numpy(data[:, self.categorical_idx].astype(np.int64))
-            data = torch.from_numpy(data[:, np.setdiff1d(np.arange(data.shape[1]), self.categorical_idx)])
-            return data, data_cat
+        assert self.model is not None, "Model should be defined"
+        #
+        # if len(y.shape) == 1:
+        #     y = y[:, np.newaxis]
+        # y = torch.from_numpy(y.astype(np.float32))
+        # if weights is not None:
+        #     weights = torch.from_numpy(weights.astype(np.float32))
 
-        elif len(self.categorical_idx) == 0:
-            data = torch.from_numpy(data)
-            return data, None
+        if not self.val_col:
+            logger.info("Validation data should be defined. No validation will be performed and C = 1 will be used")
+            return self._optimize(data, 1.0)
 
-        else:
-            data_cat = torch.from_numpy(data.astype(np.int64))
-            return None, data_cat
+        best_score = -np.inf
+        best_model = None
+        es = 0
 
-    def _optimize(
-        self,
-        train_ds: SparkDataset,
-        val_ds: SparkDataset,
-        weights_col: Optional[str] = None,
-        c: float = 1
-    ):
+        val_df = data.where(F.col(self.val_col) == 1)
+
+        for c in self.cs:
+            model = self._optimize(data, c)
+            val_pred = (
+                model
+                .transform(val_df)
+                .select(SparkDataset.ID_COLUMN, self.label_col, self.getPredictionCol())
+            )
+            score = self.metric(val_pred)
+            logger.info(f"Linear model: C = {c} score = {score}")
+            if score > best_score:
+                best_score = score
+                best_model = model
+                es = 0
+            else:
+                es += 1
+
+            if es >= self.early_stopping:
+                break
+
+        self._transformer = best_model
+
+        return self._transformer
+
+    def _optimize(self, train_df: SparkDataFrame, c: float = 1) -> Transformer:
         """Optimize single model.
 
         Args:
@@ -119,6 +166,13 @@ class SparkTorchBasedLinearEstimator:
 
         """
         # self.model.train()
+
+        numeric_feats = [feat for feat, role in self.getInputRoles().items() if isinstance(role, NumericRole)]
+        cat_feats = [feat for feat, role in self.getInputRoles().items() if isinstance(role, CategoryRole)]
+
+        numeric_assembler = VectorAssembler(inputCols=numeric_feats, outputCol="numeric_features")
+        cat_assembler = VectorAssembler(inputCols=cat_feats, outputCol="cat_features")
+
         opt = optim.LBFGS(
             self.model.parameters(),
             lr=0.1,
@@ -168,17 +222,21 @@ class SparkTorchBasedLinearEstimator:
             optimizer=opt,
             train_minibatch_fn=_train_minibatch_fn(),
             loss=lambda input, target: self._loss_fn(input, target.long(), None, c),
+            # TODO: SPARK-LAMA shapes?
             input_shapes=[[-1, 1, 28, 28]],
-            feature_cols=['features'],
-            label_cols=[train_ds.target_column],
+            feature_cols=[numeric_assembler.getOutputCol(), cat_assembler.getOutputCol()],
+            label_cols=[self.label_col],
             batch_size=128,
             epochs=self.max_iter,
-            validation=0.1,
+            # validation=0.1,
             verbose=1
         )
 
-        torch_model = torch_estimator.fit(train_ds.data).setOutputCols(['label_prob'])
-        pred_df = torch_model.transform(val_ds.data)
+        sdf = numeric_assembler.transform(train_df)
+        sdf = cat_assembler.transform(sdf)
+        torch_model = torch_estimator.fit(sdf).setOutputCols(self.getPredictionCol())
+
+        return PipelineModel(stages=[numeric_assembler, cat_assembler, torch_model])
 
     def _loss_fn(
         self,
@@ -211,93 +269,3 @@ class SparkTorchBasedLinearEstimator:
         penalty = torch.norm(all_params, 2).pow(2) / 2 / n
 
         return loss + 0.5 * penalty / c
-
-    def fit(
-        self,
-        ds: SparkDataset,
-        weights_col: Optional[str] = None
-    ):
-        """Fit method.
-
-        Args:
-            data: Data to train.
-            y: Train target values.
-            weights: Train items weights.
-            data_val: Data to validate.
-            y_val: Valid target values.
-            weights_val: Validation item weights.
-
-        Returns:
-            self.
-
-        """
-        assert self.model is not None, "Model should be defined"
-        data, data_cat = self._prepare_data(data)
-        if len(y.shape) == 1:
-            y = y[:, np.newaxis]
-        y = torch.from_numpy(y.astype(np.float32))
-        if weights is not None:
-            weights = torch.from_numpy(weights.astype(np.float32))
-
-        if data_val is None and y_val is None:
-            logger.info2("Validation data should be defined. No validation will be performed and C = 1 will be used")
-            self._optimize(data, data_cat, y, weights, 1.0)
-
-            return self
-
-        data_val, data_val_cat = self._prepare_data(data_val)
-
-        best_score = -np.inf
-        best_model = None
-        es = 0
-
-        for c in self.cs:
-            self._optimize(data, data_cat, y, weights, c)
-
-            val_pred = self._score(data_val, data_val_cat)
-            score = self.metric(y_val, val_pred, weights_val)
-            logger.info3("Linear model: C = {0} score = {1}".format(c, score))
-            if score > best_score:
-                best_score = score
-                best_model = deepcopy(self.model)
-                es = 0
-            else:
-                es += 1
-
-            if es >= self.early_stopping:
-                break
-
-        self.model = best_model
-
-        return self
-
-    def _score(self, data: np.ndarray, data_cat: Optional[np.ndarray]) -> np.ndarray:
-        """Get predicts to evaluate performance of model.
-
-        Args:
-            data: Numeric data.
-            data_cat: Categorical data.
-
-        Returns:
-            Predicted target values.
-
-        """
-        with torch.set_grad_enabled(False):
-            self.model.eval()
-            preds = self.model(data, data_cat).numpy()
-
-        return preds
-
-    def predict(self, data: np.ndarray) -> np.ndarray:
-        """Inference phase.
-
-        Args:
-            data: Data to test.
-
-        Returns:
-            Predicted target values.
-
-        """
-        data, data_cat = self._prepare_data(data)
-
-        return self._score(data, data_cat)
