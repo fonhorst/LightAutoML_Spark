63b51c3181d2b020d2bfc97ad9949b230e4f2f6f
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-03-05 20:00:51 +0300

    ---

--- lightautoml/spark/ml_algo/torch_based/linear_model.py
+++ lightautoml/spark/ml_algo/torch_based/linear_model.py
@@ -1,8 +1,10 @@
 import logging
 import sys
-from copy import deepcopy
-from typing import Sequence, Optional, Callable, List, Dict
+from typing import Sequence, Optional, Callable, Dict, List
 
+import horovod.spark.torch as hvd
+import numpy as np
+import pyspark.sql.functions as F
 import torch
 from horovod.spark.common.backend import SparkBackend
 from horovod.spark.common.store import Store
@@ -10,17 +12,13 @@ from pyspark.ml import PipelineModel, Transformer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.param.shared import HasPredictionCol
 from torch import optim
+from torch import nn
 
 from lightautoml.dataset.roles import CategoryRole, NumericRole, ColumnRole
-from lightautoml.ml_algo.torch_based.linear_model import ArrayOrSparseMatrix
+from lightautoml.ml_algo.torch_based.linear_model import CatRegression, CatLogisticRegression, CatMulticlass
 from lightautoml.spark.dataset.base import SparkDataset, SparkDataFrame
-
-import horovod.spark.torch as hvd
-import numpy as np
-
 from lightautoml.spark.transformers.base import SparkBaseEstimator
-import pyspark.sql.functions as F
-
+from lightautoml.tasks.losses import TorchLossWrapper
 
 logger = logging.getLogger(__name__)
 
@@ -32,7 +30,6 @@ class SparkTorchBasedLinearEstimator(SparkBaseEstimator, HasPredictionCol):
                  prediction_col: str,
                  prediction_role: ColumnRole,
                  val_col: Optional[str] = None,
-                 categorical_idx: Sequence[int] = (),
                  embed_sizes: Sequence[int] = (),
                  output_size: int = 1,
                  cs: Sequence[float] = (
@@ -77,7 +74,6 @@ class SparkTorchBasedLinearEstimator(SparkBaseEstimator, HasPredictionCol):
         self.val_col = val_col
         self.set(self.predictionCol, prediction_col)
 
-        self.categorical_idx = categorical_idx
         self.embed_sizes = embed_sizes
         self.output_size = output_size
 
@@ -114,12 +110,6 @@ class SparkTorchBasedLinearEstimator(SparkBaseEstimator, HasPredictionCol):
 
         """
         assert self.model is not None, "Model should be defined"
-        #
-        # if len(y.shape) == 1:
-        #     y = y[:, np.newaxis]
-        # y = torch.from_numpy(y.astype(np.float32))
-        # if weights is not None:
-        #     weights = torch.from_numpy(weights.astype(np.float32))
 
         if not self.val_col:
             logger.info("Validation data should be defined. No validation will be performed and C = 1 will be used")
@@ -165,10 +155,8 @@ class SparkTorchBasedLinearEstimator(SparkBaseEstimator, HasPredictionCol):
             c: Regularization coefficient.
 
         """
-        # self.model.train()
-
-        numeric_feats = [feat for feat, role in self.getInputRoles().items() if isinstance(role, NumericRole)]
-        cat_feats = [feat for feat, role in self.getInputRoles().items() if isinstance(role, CategoryRole)]
+        numeric_feats = self._get_numeric_feats()
+        cat_feats = self._get_cat_feats()
 
         numeric_assembler = VectorAssembler(inputCols=numeric_feats, outputCol="numeric_features")
         cat_assembler = VectorAssembler(inputCols=cat_feats, outputCol="cat_features")
@@ -269,3 +257,136 @@ class SparkTorchBasedLinearEstimator(SparkBaseEstimator, HasPredictionCol):
         penalty = torch.norm(all_params, 2).pow(2) / 2 / n
 
         return loss + 0.5 * penalty / c
+
+    def _get_numeric_feats(self) -> List[str]:
+        return [
+            feat for feat, role in self.getInputRoles().items()
+            if isinstance(role, NumericRole)
+        ]
+
+    def _get_cat_feats(self) -> List[str]:
+        return [
+            feat for feat, role in self.getInputRoles().items()
+            if isinstance(role, CategoryRole)
+        ]
+
+
+class SparkTorchBasedLinearRegression(SparkTorchBasedLinearEstimator):
+    """Torch-based linear regressor optimized by L-BFGS."""
+    def __init__(self,
+                 input_roles: Dict[str, ColumnRole],
+                 label_col: str,
+                 prediction_col: str,
+                 prediction_role: ColumnRole,
+                 val_col: Optional[str] = None,
+                 embed_sizes: Sequence[int] = (),
+                 output_size: int = 1,
+                 cs: Sequence[float] = (
+                    0.00001,
+                    0.00005,
+                    0.0001,
+                    0.0005,
+                    0.001,
+                    0.005,
+                    0.01,
+                    0.05,
+                    0.1,
+                    0.5,
+                    1.0,
+                    2.0,
+                    5.0,
+                    7.0,
+                    10.0,
+                    20.0,
+                 ),
+                 max_iter: int = 1000,
+                 tol: float = 1e-5,
+                 early_stopping: int = 2,
+                 loss=Optional[Callable],
+                 metric=Optional[Callable]):
+        """
+        Args:
+            data_size: used only for super function.
+            categorical_idx: indices of categorical features.
+            embed_sizes: categorical embedding sizes
+            output_size: size of output layer.
+            cs: regularization coefficients.
+            max_iter: maximum iterations of L-BFGS.
+            tol: the tolerance for the stopping criteria.
+            early_stopping: maximum rounds without improving.
+            loss: loss function. Format: loss(preds, true) -> loss_arr, assume reduction='none'.
+            metric: metric function. Format: metric(y_true, y_preds, sample_weight = None) -> float (greater_is_better).
+
+        """
+
+        super().__init__(input_roles, label_col, prediction_col, prediction_role, val_col, embed_sizes,
+                         output_size, cs, max_iter, tol, early_stopping, loss, metric)
+
+        numeric_feats = self._get_numeric_feats()
+
+        self.model = CatRegression(
+            len(numeric_feats),
+            self.embed_sizes,
+            self.output_size,
+        )
+
+
+class SparkTorchBasedLogisticRegression(SparkTorchBasedLinearEstimator):
+    """Linear binary classifier."""
+    def __init__(self, input_roles: Dict[str, ColumnRole], label_col: str, prediction_col: str,
+                 prediction_role: ColumnRole, val_col: Optional[str] = None, embed_sizes: Sequence[int] = (),
+                 output_size: int = 1, cs: Sequence[float] = (
+                    0.00001,
+                    0.00005,
+                    0.0001,
+                    0.0005,
+                    0.001,
+                    0.005,
+                    0.01,
+                    0.05,
+                    0.1,
+                    0.5,
+                    1.0,
+                    2.0,
+                    5.0,
+                    7.0,
+                    10.0,
+                    20.0,
+            ), max_iter: int = 1000, tol: float = 1e-5, early_stopping: int = 2, loss=Optional[Callable],
+                 metric=Optional[Callable]):
+        """
+        Args:
+            data_size: not used.
+            categorical_idx: indices of categorical features.
+            embed_sizes: categorical embedding sizes.
+            output_size: size of output layer.
+            cs: regularization coefficients.
+            max_iter: maximum iterations of L-BFGS.
+            tol: the tolerance for the stopping criteria.
+            early_stopping: maximum rounds without improving.
+            loss: loss function. Format: loss(preds, true) -> loss_arr, assume reduction='none'.
+            metric: metric function. Format: metric(y_true, y_preds, sample_weight = None) -> float (greater_is_better).
+
+        """
+        if output_size == 1:
+            _loss = nn.BCELoss
+            _model = CatLogisticRegression
+            self._binary = True
+        else:
+            _loss = nn.CrossEntropyLoss
+            _model = CatMulticlass
+            self._binary = False
+
+        if loss is None:
+            loss = TorchLossWrapper(_loss)
+
+        super().__init__(input_roles, label_col, prediction_col, prediction_role, val_col, embed_sizes, output_size, cs,
+                         max_iter, tol, early_stopping, loss, metric)
+
+        numeric_feats = self._get_numeric_feats()
+
+        self.model = _model(
+            len(numeric_feats),
+            self.embed_sizes,
+            self.output_size,
+        )
