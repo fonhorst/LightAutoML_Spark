a85beb8a67c6a58894b5d1084f79cb52e9ca4aca
Azamat G <mr.g.azamat@gmail.com>
2022-03-15 19:43:31 +0300

    Add example to run automl.get_feature_scores()

--- examples/spark/pdp.py
+++ examples/spark/pdp.py
@@ -1,12 +1,9 @@
 import datetime
 import logging.config
-from typing import List, Tuple
 
-import numpy as np
-import pandas as pd
 from pyspark.ml import Transformer, PipelineModel
-from pyspark.sql import Window, functions as F, types as SparkTypes
-from tqdm import tqdm
+from pyspark.sql import functions as F
+from lightautoml.spark.automl.presets.tabular_presets import SparkTabularAutoML
 from lightautoml.spark.automl.presets.utils import replace_dayofweek_in_date, replace_month_in_date, replace_year_in_date
 from lightautoml.spark.dataset.base import SparkDataFrame
 
@@ -25,149 +22,6 @@ class ConstantTransformer(Transformer):
 
     def _transform(self, df: SparkDataFrame):
         return df.select(*df.columns, F.lit(1).alias("prediction"))
-
-
-def _get_histogram(data: SparkDataFrame, column: str, n_bins: int) -> Tuple[List, np.ndarray]:
-    assert n_bins >= 2, "n_bins must be equal 2 or more"
-    bin_edges, counts = data \
-        .select(F.col(column).cast("double")) \
-        .where(F.col(column).isNotNull()) \
-        .rdd.map(lambda x : x[0]) \
-        .histogram(n_bins)
-    bin_edges = np.array(bin_edges)
-    return counts, bin_edges
-
-
-def get_pdp_data_numeric_feature(df: SparkDataFrame,
-                                 feature_name: str,
-                                 model: PipelineModel,
-                                 prediction_col: str,
-                                 n_bins: int) -> Tuple[List, List, List]:
-    counts, bin_edges = _get_histogram(df, feature_name, n_bins)
-    grid = (bin_edges[:-1] + bin_edges[1:]) / 2
-    ys = []
-    for i in tqdm(grid):
-        # replace feature column values with constant
-        sdf = df.select(*[c for c in df.columns if c != feature_name], F.lit(i).alias(feature_name))
-
-        # infer via transformer
-        preds = model.transform(sdf)
-        preds.show(truncate=False)
-        preds = np.array(preds.select(prediction_col).collect())
-        ys.append(preds)
-    return grid, ys, counts
-
-
-def get_pdp_data_categorical_feature(df: SparkDataFrame,
-                                     feature_name: str,
-                                     model: PipelineModel,
-                                     prediction_col: str,
-                                     n_top_cats: int) -> Tuple[List, List, List]:
-    """Returns grid, ys, counts to plot PDP
-
-    Args:
-        df (SparkDataFrame): Spark DataFrame with `feature_name` column
-        feature_name (str): feature column name
-        model (PipelineModel): Spark Pipeline Model
-        prediction_col (str): prediction column to be created by the `model`
-        n_top_cats (int): param to selection top n categories
-    
-    Returns:
-        Tuple[List, List, List]:
-        `grid` is list of categories,
-        `ys` is list of predictions by category,
-        `counts` is numbers of values by category
-    """
-    feature_cnt = df.groupBy(feature_name).count().orderBy(F.desc("count")).collect()
-    grid = [row[feature_name] for row in feature_cnt[:n_top_cats]]
-    counts = [row["count"] for row in feature_cnt[:n_top_cats]]
-    ys = []
-    for i in tqdm(grid):
-        sdf = df.select(*[c for c in df.columns if c != feature_name], F.lit(i).alias(feature_name))
-        preds = model.transform(sdf)
-        preds = np.array(preds.select(prediction_col).collect())
-        ys.append(preds)
-    if len(feature_cnt) > n_top_cats:
-
-        # unique other categories
-        unique_other_categories = [row[feature_name] for row in feature_cnt[n_top_cats:]]
-
-        # get non-top categories, natural distributions is important here
-        w = Window().orderBy(F.lit('A'))  # window without sorting
-        other_categories_collection = df.select(feature_name) \
-            .filter(F.col(feature_name).isin(unique_other_categories)) \
-            .select(F.row_number().over(w).alias("row_num"), feature_name) \
-            .collect()
-        
-        # dict with key=%row number% and value=%category%
-        other_categories_dict = {x["row_num"]: x[feature_name] for x in other_categories_collection}
-        max_row_num = len(other_categories_collection)
-
-        def get_category_by_row_num(row_num):
-            if (remainder := row_num % max_row_num) == 0:
-                key = row_num
-            else:
-                key = remainder
-            return other_categories_dict[key]
-        get_category_udf = F.udf(get_category_by_row_num, SparkTypes.StringType())
-
-        # add row number to main dataframe and exclude feature_name column
-        sdf = df.select(F.row_number().over(w).alias("row_num"), *[f for f in df.columns if f != feature_name])
-
-        all_columns_except_row_num = [f for f in sdf.columns if f != "row_num"]
-        feature_col = get_category_udf(F.col("row_num")).alias(feature_name)
-        # exclude row number from dataframe
-        # and add back feature_name column filled with other categories same distribution
-        sdf = sdf.select(*all_columns_except_row_num, feature_col)
-
-        preds = model.transform(sdf)
-        preds = np.array(preds.select(prediction_col).collect())
-
-        grid.append("<OTHER>")
-        ys.append(preds)
-        counts.append(sum([row["count"] for row in feature_cnt[n_top_cats:]]))
-
-    return grid, ys, counts
-
-
-def get_pdp_data_datetime_feature(df: SparkDataFrame,
-                                  feature_name: str,
-                                  model: PipelineModel,
-                                  prediction_col: str,
-                                  datetime_level: str) -> Tuple[List, List, List]:
-    # test_data_read = self.reader.read(df)
-    if datetime_level == "year":
-        feature_cnt = df.groupBy(F.year(feature_name).alias("year")).count().orderBy(F.asc("year")).collect()
-        grid = [x["year"] for x in feature_cnt]
-        counts = [row["count"] for row in feature_cnt]
-        replace_date_element_udf = F.udf(replace_year_in_date, SparkTypes.DateType())
-    elif datetime_level == "month":
-        feature_cnt = df.groupBy(F.month(feature_name).alias("month")).count().orderBy(F.asc("month")).collect()
-        grid = np.arange(1, 13)
-        grid = grid.tolist()
-        counts = [0] * 12
-        for row in feature_cnt:
-            counts[row["month"]-1] = row["count"]
-        replace_date_element_udf = F.udf(replace_month_in_date, SparkTypes.DateType())
-    else:
-        feature_cnt = df.groupBy(F.dayofweek(feature_name).alias("dayofweek")).count().orderBy(F.asc("dayofweek")).collect()
-        grid = np.arange(7)
-        grid = grid.tolist()
-        counts = [0] * 7
-        for row in feature_cnt:
-            counts[row["dayofweek"]-1] = row["count"]
-        replace_date_element_udf = F.udf(replace_dayofweek_in_date, SparkTypes.DateType())
-    ys = []
-    for i in tqdm(grid):
-        all_columns_except_feature = [c for c in df.columns if c != feature_name]
-        feature_col = replace_date_element_udf(F.col(feature_name), F.lit(i)).alias(feature_name)
-        sdf = df.select(*all_columns_except_feature, feature_col)
-        preds = model.transform(sdf)
-        preds.show(truncate=False)
-        preds = np.array(preds.select(prediction_col).collect())
-        ys.append(preds)
-
-    return grid, ys, counts
 
 
 if __name__ == "__main__":
@@ -193,13 +47,12 @@ if __name__ == "__main__":
 
         # ===========================================
         pipeline_model = PipelineModel(stages=[ConstantTransformer()])
-        # get_pdp_data_categorical_feature(df, "category_feature", pipeline_model, "prediction", 2)
+        # SparkTabularAutoML.get_pdp_data_categorical_feature(df, "category_feature", pipeline_model, "prediction", 2)
 
-        # get_pdp_data_numeric_feature(df, "numeric_feature", pipeline_model, "prediction", 2)
+        # SparkTabularAutoML.get_pdp_data_numeric_feature(df, "numeric_feature", pipeline_model, "prediction", 2)
 
-        # get_pdp_data_datetime_feature(df, "datetime_feature", pipeline_model, "prediction", "year")
-        get_pdp_data_datetime_feature(df, "datetime_feature", pipeline_model, "prediction", "month")
-        # get_pdp_data_datetime_feature(df, "datetime_feature", pipeline_model, "prediction", "dayofweek")
-
+        SparkTabularAutoML.get_pdp_data_datetime_feature(df, "datetime_feature", pipeline_model, "prediction", "year")
+        # SparkTabularAutoML.get_pdp_data_datetime_feature(df, "datetime_feature", pipeline_model, "prediction", "month")
+        # SparkTabularAutoML.get_pdp_data_datetime_feature(df, "datetime_feature", pipeline_model, "prediction", "dayofweek")
 
         logger.info("Finished")
