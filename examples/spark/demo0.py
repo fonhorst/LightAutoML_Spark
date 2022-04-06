f9580546367996be95c776aefe97dc4e84955763
Azamat G <mr.g.azamat@gmail.com>
2022-04-06 16:08:30 +0300

    Add spark demo0.py and demo3.py

---
+++ examples/spark/demo0.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+building ML pipeline from blocks and fit + predict the pipeline itself
+"""
+
+import os
+import pickle
+import time
+
+import numpy as np
+import pandas as pd
+
+from lightautoml.dataset.np_pd_dataset import PandasDataset
+from lightautoml.dataset.roles import CategoryRole
+from lightautoml.dataset.roles import DatetimeRole
+from lightautoml.dataset.roles import FoldsRole
+from lightautoml.dataset.roles import NumericRole
+from lightautoml.dataset.roles import TargetRole
+from lightautoml.dataset.utils import roles_parser
+from lightautoml.ml_algo.tuning.optuna import OptunaTuner
+
+
+from lightautoml.pipelines.selection.importance_based import (
+    ImportanceCutoffSelector,
+    ModelBasedImportanceEstimator,
+)
+from lightautoml.spark.dataset.base import SparkDataset
+from lightautoml.spark.ml_algo.boost_lgbm import SparkBoostLGBM
+from lightautoml.spark.pipelines.features.lgb_pipeline import SparkLGBSimpleFeatures
+from lightautoml.spark.pipelines.ml.base import SparkMLPipeline
+from lightautoml.spark.reader.base import SparkToSparkReader
+from lightautoml.spark.tasks.base import SparkTask
+from lightautoml.spark.validation.iterators import SparkFoldsIterator
+
+from examples_utils import get_spark_session
+from pyspark.sql import functions as F
+
+
+if __name__ == "__main__":
+    spark = get_spark_session()
+
+    # Read data from file
+    print("Read data from file")
+    data = pd.read_csv(
+        "examples/data/sampled_app_train.csv",
+        usecols=[
+            "TARGET",
+            "NAME_CONTRACT_TYPE",
+            "AMT_CREDIT",
+            "NAME_TYPE_SUITE",
+            "AMT_GOODS_PRICE",
+            "DAYS_BIRTH",
+            "DAYS_EMPLOYED",
+        ],
+    )
+
+    # Fix dates and convert to date type
+    print("Fix dates and convert to date type")
+    # data["BIRTH_DATE"] = np.datetime64("2018-01-01") + data["DAYS_BIRTH"].astype(np.dtype("timedelta64[D]"))
+    # data["EMP_DATE"] = np.datetime64("2018-01-01") + np.clip(data["DAYS_EMPLOYED"], None, 0).astype(
+    #     np.dtype("timedelta64[D]")
+    # )
+    data.drop(["DAYS_BIRTH", "DAYS_EMPLOYED"], axis=1, inplace=True)
+
+    # Create folds
+    print("Create folds")
+    data["__fold__"] = np.random.randint(0, 5, len(data))
+
+    # Print data head
+    print("Print data head")
+    print(data.head())
+
+    dataset_sdf = spark.createDataFrame(data)
+    dataset_sdf = dataset_sdf.select(
+        '*',
+        F.monotonically_increasing_id().alias(SparkDataset.ID_COLUMN)
+    ).cache()
+    dataset_sdf.write.mode('overwrite').format('noop').save()
+    dataset_sdf = dataset_sdf.select(F.col("__fold__").cast("int").alias("__fold__"), *[c for c in dataset_sdf.columns if c != "__fold__"])
+    
+
+    # # Set roles for columns
+    print("Set roles for columns")
+    check_roles = {
+        TargetRole(): "TARGET",
+        CategoryRole(dtype=str): ["NAME_CONTRACT_TYPE", "NAME_TYPE_SUITE"],
+        NumericRole(np.float32): ["AMT_CREDIT", "AMT_GOODS_PRICE"],
+        # DatetimeRole(seasonality=["y", "m", "wd"]): ["BIRTH_DATE", "EMP_DATE"],
+        FoldsRole(): "__fold__",
+    }
+
+    # create Task
+    task = SparkTask("binary")
+    cacher_key = "main_cache"
+
+    # # Creating PandasDataSet
+    print("Creating PandasDataset")
+    start_time = time.time()
+    # pd_dataset = PandasDataset(data, roles_parser(check_roles), task=task)
+    sreader = SparkToSparkReader(task=task, advanced_roles=False)
+    sdataset = sreader.fit_read(dataset_sdf, roles=check_roles)
+    print("PandasDataset created. Time = {:.3f} sec".format(time.time() - start_time))
+
+    # # Print pandas dataset feature roles
+    print("Print pandas dataset feature roles")
+    roles = sdataset.roles
+    for role in roles:
+        print("{}: {}".format(role, roles[role]))
+
+    # # Feature selection part
+    print("Feature selection part")
+    selector_iterator = SparkFoldsIterator(sdataset, 1)
+    print("Selection iterator created")
+
+    pipe = SparkLGBSimpleFeatures(cacher_key='preselector')
+    print("Pipe and model created")
+
+    model0 = SparkBoostLGBM(
+        cacher_key='preselector',
+        default_params={
+            "learningRate": 0.05,
+            "numLeaves": 64,
+            # "seed": 0,
+            "numThreads": 5,
+        }
+    )
+
+    mbie = ModelBasedImportanceEstimator()
+    selector = ImportanceCutoffSelector(pipe, model0, mbie, cutoff=10)
+    start_time = time.time()
+    selector.fit(selector_iterator)
+    print("Feature selector fitted. Time = {:.3f} sec".format(time.time() - start_time))
+
+    print("Feature selector scores:")
+    print("\n{}".format(selector.get_features_score()))
+
+    # # Build AutoML pipeline
+    print("Start building AutoML pipeline")
+    pipe = SparkLGBSimpleFeatures(cacher_key=cacher_key)
+    print("Pipe created")
+
+    params_tuner1 = OptunaTuner(n_trials=10, timeout=300)
+    model1 = SparkBoostLGBM(cacher_key=cacher_key, default_params={"learningRate": 0.05, "numLeaves": 128})
+    print("Tuner1 and model1 created")
+
+    params_tuner2 = OptunaTuner(n_trials=100, timeout=300)
+    model2 = SparkBoostLGBM(cacher_key=cacher_key, default_params={"learningRate": 0.025, "numLeaves": 64})
+    print("Tuner2 and model2 created")
+
+    total = SparkMLPipeline(
+        cacher_key=cacher_key,
+        ml_algos=[(model1, params_tuner1), (model2, params_tuner2)],
+        pre_selection=selector,
+        features_pipeline=pipe,
+        post_selection=None,
+    )
+
+    print("Finished building AutoML pipeline")
+
+    # # Create full train iterator
+    print("Full train valid iterator creation")
+    train_valid = SparkFoldsIterator(sdataset)
+    print("Full train valid iterator created")
+
+    # # Fit predict using pipeline
+    print("Start AutoML pipeline fit_predict")
+    start_time = time.time()
+    pred = total.fit_predict(train_valid)
+    print("Fit_predict finished. Time = {:.3f} sec".format(time.time() - start_time))
+
+    # # Check preds
+    print("Preds:")
+    print("\n{}".format(pred))
+    print("Preds.shape = {}".format(pred.shape))
+
+    # # Predict full train dataset
+    print("Predict full train dataset")
+    start_time = time.time()
+    train_pred = total.predict(sdataset)
+    print("Predict finished. Time = {:.3f} sec".format(time.time() - start_time))
+    print("Preds:")
+    print("\n{}".format(train_pred))
+    print("Preds.shape = {}".format(train_pred.shape))
+
+    # print("Pickle automl")
+    # with open("automl.pickle", "wb") as f:
+    #     pickle.dump(total, f)
+
+    # print("Load pickled automl")
+    # with open("automl.pickle", "rb") as f:
+    #     total = pickle.load(f)
+
+    # print("Predict loaded automl")
+    # train_pred = total.predict(sdataset)
+    # os.remove("automl.pickle")
+
+    # # # Check preds feature names
+    # print("Preds features: {}".format(train_pred.features))
+
+    # # Check model feature scores
+    print("Feature scores for model_1:\n{}".format(model1.get_features_score()))
+    print("Feature scores for model_2:\n{}".format(model2.get_features_score()))
---
+++ examples/spark/demo3.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+
+from lightautoml.automl.base import AutoML
+from lightautoml.ml_algo.boost_lgbm import BoostLGBM
+from lightautoml.ml_algo.tuning.optuna import OptunaTuner
+from lightautoml.pipelines.features.lgb_pipeline import LGBSimpleFeatures
+from lightautoml.pipelines.ml.base import MLPipeline
+from lightautoml.pipelines.selection.base import ComposedSelector
+from lightautoml.pipelines.selection.importance_based import ImportanceCutoffSelector
+from lightautoml.pipelines.selection.importance_based import (
+    ModelBasedImportanceEstimator,
+)
+from lightautoml.pipelines.selection.permutation_importance_based import (
+    NpIterativeFeatureSelector,
+)
+from lightautoml.pipelines.selection.permutation_importance_based import (
+    NpPermutationImportanceEstimator,
+)
+from lightautoml.reader.base import PandasToPandasReader
+from lightautoml.spark.automl.base import SparkAutoML
+from lightautoml.spark.automl.presets.tabular_presets import SparkTabularAutoML
+from lightautoml.spark.ml_algo.boost_lgbm import SparkBoostLGBM
+from lightautoml.spark.pipelines.features.lgb_pipeline import SparkLGBSimpleFeatures
+from lightautoml.spark.pipelines.ml.base import SparkMLPipeline
+from lightautoml.spark.pipelines.selection.permutation_importance_based import SparkNpPermutationImportanceEstimator
+from lightautoml.spark.reader.base import SparkToSparkReader
+from lightautoml.spark.tasks.base import SparkTask
+from lightautoml.tasks import Task
+
+from examples_utils import get_spark_session
+from pyspark.sql import functions as F
+
+
+if __name__ == "__main__":
+    spark = get_spark_session()
+
+    np.random.seed(42)
+
+    print("Load data...")
+    data = pd.read_csv("examples/data/sampled_app_train.csv")
+    print("Data loaded")
+
+    print("Features modification from user side...")
+    data["BIRTH_DATE"] = (np.datetime64("2018-01-01") + data["DAYS_BIRTH"].astype(np.dtype("timedelta64[D]"))).astype(str)
+    data["EMP_DATE"] = (
+        np.datetime64("2018-01-01") + np.clip(data["DAYS_EMPLOYED"], None, 0).astype(np.dtype("timedelta64[D]"))
+    ).astype(str)
+
+    data["constant"] = 1
+    data["allnan"] = np.nan
+
+    data.drop(["DAYS_BIRTH", "DAYS_EMPLOYED"], axis=1, inplace=True)
+    print("Features modification finished")
+
+    print("Split data...")
+    train_data, test_data = train_test_split(data, test_size=2000, stratify=data["TARGET"], random_state=13)
+    train_data.reset_index(drop=True, inplace=True)
+    test_data.reset_index(drop=True, inplace=True)
+    print("Data splitted. Parts sizes: train_data = {}, test_data = {}".format(train_data.shape, test_data.shape))
+
+
+    train_data_sdf = spark.createDataFrame(train_data).cache()
+    test_data_sdf = spark.createDataFrame(test_data).cache()
+    train_data_sdf.write.mode('overwrite').format('noop').save()
+    test_data_sdf.write.mode('overwrite').format('noop').save()
+    
+
+    print("Create task..")
+    task = SparkTask("binary")
+    print("Task created")
+
+    cacher_key = "main_cache"
+
+    print("Create reader...")
+    # reader = PandasToPandasReader(task, cv=5, random_state=1)
+    sreader = SparkToSparkReader(task=task, cv=5, random_state=1, advanced_roles=False)
+    print("Reader created")
+
+    # selector parts
+    print("Create feature selector")
+    model01 = SparkBoostLGBM(
+        cacher_key='preselector',
+        default_params={
+            "learningRate": 0.05,
+            "numLeaves": 64,
+            # "seed": 42,
+            "numThreads": 5,
+        }
+    )
+    model02 = SparkBoostLGBM(
+        cacher_key='preselector',
+        default_params={
+            "learningRate": 0.05,
+            "numLeaves": 64,
+            # "seed": 42,
+            "numThreads": 5,
+        }
+    )
+    pipe0 = SparkLGBSimpleFeatures(cacher_key='preselector')
+    pie = SparkNpPermutationImportanceEstimator()
+    pie1 = ModelBasedImportanceEstimator()
+    sel1 = ImportanceCutoffSelector(pipe0, model01, pie1, cutoff=0)
+    sel2 = NpIterativeFeatureSelector(pipe0, model02, pie, feature_group_size=1, max_features_cnt_in_result=15)
+    selector = ComposedSelector([sel1, sel2])
+    print("Feature selector created")
+
+    # pipeline 1 level parts
+    print("Start creation pipeline_1...")
+    pipe = SparkLGBSimpleFeatures(cacher_key=cacher_key)
+
+    print("\t ParamsTuner1 and Model1...")
+    params_tuner1 = OptunaTuner(n_trials=100, timeout=100)
+    model1 = SparkBoostLGBM(
+        cacher_key=cacher_key,
+        default_params={
+            "learningRate": 0.05,
+            "numLeaves": 128,
+            # "seed": 1,
+            "numThreads": 5,
+        }
+    )
+    print("\t Tuner1 and model1 created")
+
+    print("\t ParamsTuner2 and Model2...")
+    model2 = SparkBoostLGBM(
+        cacher_key=cacher_key,
+        default_params={
+            "learningRate": 0.025,
+            "numLeaves": 64,
+            # "seed": 2,
+            "numThreads": 5,
+        }
+    )
+    print("\t Tuner2 and model2 created")
+
+    print("\t Pipeline1...")
+    pipeline_lvl1 = SparkMLPipeline(
+        cacher_key=cacher_key,
+        ml_algos=[(model1, params_tuner1), model2],
+        pre_selection=selector,
+        features_pipeline=pipe,
+        post_selection=None,
+    )
+    print("Pipeline1 created")
+
+    # pipeline 2 level parts
+    print("Start creation pipeline_2...")
+    pipe1 = SparkLGBSimpleFeatures(cacher_key=cacher_key)
+
+    print("\t ParamsTuner and Model...")
+    model = SparkBoostLGBM(
+        cacher_key=cacher_key,
+        default_params={
+            "learningRate": 0.05,
+            "numLeaves": 64,
+            "maxBin": 1024,
+            # "seed": 3,
+            "numThreads": 5,
+        }
+    )
+    print("\t Tuner and model created")
+
+    print("\t Pipeline2...")
+    pipeline_lvl2 = SparkMLPipeline(
+        cacher_key=cacher_key,
+        ml_algos=[model],
+        pre_selection=None,
+        features_pipeline=pipe1,
+        post_selection=None
+    )
+    print("Pipeline2 created")
+
+    print("Create AutoML pipeline...")
+    automl = SparkAutoML(
+        sreader,
+        [
+            [pipeline_lvl1],
+            [pipeline_lvl2],
+        ],
+        skip_conn=False
+    )
+
+    print("AutoML pipeline created...")
+
+    print("Start AutoML pipeline fit_predict...")
+    start_time = time.time()
+    oof_pred = automl.fit_predict(train_data_sdf, roles={"target": "TARGET"})
+    print("AutoML pipeline fitted and predicted. Time = {:.3f} sec".format(time.time() - start_time))
+
+    print("Feature importances of selector:\n{}".format(selector.get_features_score()))
+
+    print("oof_pred:\n{}\nShape = {}".format(oof_pred, oof_pred.shape))
+
+    print("Feature importances of top level algorithm:\n{}".format(automl.levels[-1][0].ml_algos[0].get_features_score()))
+
+    print(
+        "Feature importances of lowest level algorithm - model 0:\n{}".format(
+            automl.levels[0][0].ml_algos[0].get_features_score()
+        )
+    )
+
+    # print(
+    #     "Feature importances of lowest level algorithm - model 1:\n{}".format(
+    #         automl.levels[0][0].ml_algos[1].get_features_score()
+    #     )
+    # )
+
+    test_pred = automl.predict(test_data_sdf)
+    print("Prediction for test data:\n{}\nShape = {}".format(test_pred, test_pred.shape))
+
+    print("Check scores...")
+    # print("OOF score: {}".format(roc_auc_score(train_data_sdf["TARGET"].values, oof_pred.data[:, 0])))
+    # print("TEST score: {}".format(roc_auc_score(test_data_sdf["TARGET"].values, test_pred.data[:, 0])))
