2c0d518ce09ba2749c7074cb9025f1cf81a4ff0a
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-04-11 17:04:24 +0300

    ---

--- examples/spark/tabular-preset-automl.py
+++ examples/spark/tabular-preset-automl.py
@@ -25,8 +25,8 @@ def main(spark: SparkSession, dataset_name: str, seed: int):
     # 2. use_algos = [["linear_l2"]]
     # 3. use_algos = [["lgb", "linear_l2"], ["lgb"]]
     # use_algos = [["lgb", "linear_l2"], ["lgb"]]
-    use_algos = [["lgb"]]
-    # use_algos = [["linear_l2"]]
+    # use_algos = [["lgb"]]
+    use_algos = [["linear_l2", "lgb"]]
     cv = 5
     path, task_type, roles, dtype = get_dataset_attrs(dataset_name)
 
