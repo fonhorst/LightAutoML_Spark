4f6444c8cf7cc54e314c35ab4219e3f0772471db
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-04-25 20:41:22 +0300

    ---

--- scala-lightautoml-transformers/src/main/scala/org/apache/spark/ml/feature/lightautoml/Temp.scala
+++ scala-lightautoml-transformers/src/main/scala/org/apache/spark/ml/feature/lightautoml/Temp.scala
@@ -37,7 +37,14 @@ class BalancedUnionPartitionCoalescer extends PartitionCoalescer with Serializab
 object Temp extends App {
   println("Hello")
 
-  val spark = SparkSession.builder().master("local[2]").getOrCreate()
+  val spark = SparkSession
+          .builder()
+          .master("local-cluster[3, 2, 3072]")
+          .config("spark.driver.host", "127.0.0.1")
+          .config("spark.jars", "target/scala-2.12/spark-lightautoml_2.12-0.1.jar")
+          .getOrCreate()
+//          .config("spark.jars", "/home/nikolay/.ivy2/jars").getOrCreate()
+//  val spark = SparkSession.builder().master("local[2]").getOrCreate()
 
   import spark.sqlContext.implicits._
 
@@ -51,7 +58,16 @@ object Temp extends App {
     partitionCoalescer = Some(new BalancedUnionPartitionCoalescer)
   )
 
-  val coalesced_df = spark.createDataFrame(coalesced_rdd, schema = full_df.schema)
+  coalesced_rdd.count()
 
+//  val coalesced_df = spark.createDataFrame(coalesced_rdd, schema = full_df.schema)
+//
+//  coalesced_df.count()
+  // check for balanced dataset:
+  // 1. all executors should have the same number partitions as their parents dataset have
+  // 2. all executors should have approximately the same number of records
+  // 2. all partitions should have approximately the same number of records
+
+  // there should be an error if datasets participating in union have different number of partitions
   spark.stop()
 }
