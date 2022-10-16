40a0c5aed722908ba7dcbabb96e103e4c1cc234c
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-10-16 19:37:01 +0300

    reworked PersistenceManager to be able to work with plain Spark DataFrames

--- sparklightautoml/dataset/persistence.py
+++ sparklightautoml/dataset/persistence.py
@@ -1,10 +1,10 @@
-from abc import ABC
+import uuid
 from dataclasses import dataclass
 from typing import Dict, Optional, Callable, Union, cast, List
 
 from pyspark.sql import SparkSession
 
-from sparklightautoml.dataset.base import SparkDataset, Dependency, DepIdentifable
+from sparklightautoml.dataset.base import SparkDataset
 from sparklightautoml.utils import SparkDataFrame
 
 PersistenceIdentifable = Union[str, SparkDataset]
@@ -13,7 +13,6 @@ PersistenceIdentifable = Union[str, SparkDataset]
 @dataclass(frozen=True)
 class PersistableDataset:
     dataset: SparkDataset
-    custom_persistence: bool = False
     callback: Optional[Callable] = None
 
     @property
@@ -21,66 +20,70 @@ class PersistableDataset:
         return self.dataset.uid
 
 
+@dataclass(frozen=True)
+class PersistableDataFrame:
+    sdf: SparkDataFrame
+    uid: str
+    callback: Optional[Callable] = None
+    base_dataset: Optional[SparkDataset] = None
+
+    def to_dataset(self) -> SparkDataset:
+        assert self.base_dataset
+        ds = self.base_dataset.empty()
+        ds.set_data(self.sdf, self.base_dataset.features, self.base_dataset.roles, self.base_dataset.dependencies)
+        return ds
+
+
 # TODO: SLAMA - add documentation
 class PersistenceManager:
+
+    @staticmethod
+    def to_persistable_dataframe(dataset: SparkDataset) -> PersistableDataFrame:
+        # we intentially create new uid to use to distinguish a persisted and unpersisted dataset
+        return PersistableDataFrame(dataset.data, uid=str(uuid.uuid4()), dataset=dataset)
+
     def __init__(self, parent: Optional['PersistenceManager'] = None):
-        self._persistence_registry: Dict[str, PersistableDataset] = dict()
-        self._persisted_datasets: Dict[str, str] = dict()
+        self._persistence_registry: Dict[str, PersistableDataFrame] = dict()
         self._parent = parent
         self._children: List['PersistenceManager'] = []
 
-    def persist(self, dataset: Union[SparkDataset, PersistableDataset]) -> SparkDataset:
-        # assert dataset.uid not in self._persisted_datasets or self._persisted_datasets[dataset.uid] == name, \
-        #     f"Cannot persist the same dataset with different names. Called with name: {name}. " \
-        #     f"Already exists in the registry: {self._persisted_datasets[dataset.uid]}"
+    def persist(self, dataset: Union[SparkDataset, PersistableDataFrame]) -> PersistableDataFrame:
+        persisted_dataframe = self.to_persistable_dataframe(dataset) if isinstance(dataset, SparkDataset) \
+            else cast(PersistableDataFrame, dataset)
 
-        persisted_dataset = PersistableDataset(dataset) if isinstance(dataset, SparkDataset) \
-            else cast(PersistableDataset, dataset)
+        if persisted_dataframe.uid in self._persistence_registry:
+            return self._persistence_registry[persisted_dataframe.uid]
 
-        if persisted_dataset.uid in self._persistence_registry:
-            return self._persistence_registry[persisted_dataset.uid].dataset
-
-        if not persisted_dataset.custom_persistence:
-            deps = persisted_dataset.dataset.dependencies
-            persisted_dataset = self._persist(persisted_dataset.dataset)
+        if not persisted_dataframe.callback:
+            deps = persisted_dataframe.base_dataset.dependencies
+            persisted_dataframe = self._persist(persisted_dataframe)
             for dep in deps:
                 self.unpersist(dep)
 
-        self._persistence_registry[persisted_dataset.uid] = persisted_dataset
+        self._persistence_registry[persisted_dataframe.uid] = persisted_dataframe
 
-        return persisted_dataset.dataset
+        return persisted_dataframe
 
-    def unpersist(self, identifier: DepIdentifable):
-        uid = identifier.uid if isinstance(identifier, SparkDataset) else cast(str, identifier)
+    def unpersist(self, uid: str):
         persisted_dataset = self._persistence_registry.get(uid, None)
 
         if not persisted_dataset:
             return
 
-        if persisted_dataset.custom_persistence:
+        if persisted_dataset.callback:
             persisted_dataset.callback()
         else:
             self._unpersist(persisted_dataset)
 
         del self._persistence_registry[persisted_dataset.uid]
 
-    def unpersist_all(self, exceptions: Optional[Union[DepIdentifable, List[DepIdentifable]]] = None):
-        if exceptions:
-            if not isinstance(exceptions, list):
-                exceptions = [exceptions]
-
-            names_to_save = {
-                ex if isinstance(ex, str) else self._persisted_datasets.get(ex.uid, None)
-                for ex in exceptions
-            }
-            names = [name for name in self._persistence_registry if name not in names_to_save]
-        else:
-            names = list(self._persistence_registry)
-
+    def unpersist_all(self):
         self.unpersist_children()
 
-        for name in names:
-            self.unpersist(name)
+        uids = list(self._persistence_registry.keys())
+
+        for uid in uids:
+            self.unpersist(uid)
 
     def unpersist_children(self):
         for child in self._children:
@@ -92,15 +95,11 @@ class PersistenceManager:
         self._children.append(a_child)
         return a_child
     
-    def _persist(self, dataset: SparkDataset) -> PersistableDataset:
-        ds = SparkSession.getActiveSession().createDataFrame(dataset.data.rdd, schema=dataset.data.schema).cache()
+    def _persist(self, pdf: PersistableDataFrame) -> PersistableDataFrame:
+        ds = SparkSession.getActiveSession().createDataFrame(pdf.sdf.rdd, schema=pdf.sdf.schema).cache()
         ds.write.mode('overwrite').format('noop').save()
 
-        # TODO: need to save info about how it has been cached
-        ps_dataset = dataset.empty()
-        ps_dataset.set_data(ds, dataset.features, dataset.roles)
+        return PersistableDataFrame(ds, pdf.uid, pdf.callback, pdf.base_dataset)
 
-        return PersistableDataset(dataset)
-
-    def _unpersist(self, persisted_dataset: PersistableDataset):
-        persisted_dataset.dataset.data.unpersist()
+    def _unpersist(self, pdf: PersistableDataFrame):
+        pdf.sdf.unpersist()
