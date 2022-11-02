b8a1e5bdff752126837d0e13d399e8cbbcbb5d62
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-11-02 22:24:04 +0300

    added full test of TargetEncoderTransformer

--- scala-lightautoml-transformers/src/test/scala/org/apache/spark/ml/feature/lightautoml/TestTransformers.scala
+++ scala-lightautoml-transformers/src/test/scala/org/apache/spark/ml/feature/lightautoml/TestTransformers.scala
@@ -2,12 +2,14 @@ package org.apache.spark.ml.feature.lightautoml
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.types.{DoubleType, IntegerType, StructField, StructType}
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers._
 
+import java.io.File
 import scala.collection.JavaConverters._
+import scala.reflect.io.Directory
 
 
 abstract class BaseFunSuite extends AnyFunSuite with BeforeAndAfterAll with Logging {
@@ -21,6 +23,8 @@ abstract class BaseFunSuite extends AnyFunSuite with BeforeAndAfterAll with Logg
 
   var spark: SparkSession = _
 
+  val workdir: String = "/tmp/test_transformers"
+
   protected override def beforeAll(): Unit = {
     spark =
       SparkSession
@@ -28,10 +32,17 @@ abstract class BaseFunSuite extends AnyFunSuite with BeforeAndAfterAll with Logg
               .appName("test")
               .master("local[1]")
               .getOrCreate()
+
+
+    val dir = new Directory(new File(workdir))
+    dir.createDirectory()
   }
 
   protected override def afterAll(): Unit = {
     spark.stop()
+
+    val dir = new Directory(new File(workdir))
+    dir.deleteRecursively()
   }
 }
 
@@ -81,26 +92,80 @@ class TestLAMLStringIndexer extends BaseFunSuite {
     val in_cols = Seq("a", "b", "c").toArray
     val out_cols = in_cols.map(x => s"te_$x")
 
-    val enc = in_cols
-            .zipWithIndex.map{case (col, idx) => (col,(idx until idx + 4).map(_.toDouble).toArray)}
-            .toMap
+    val enc = Map(
+      ("a", Array(0.0, -1.0, -2.0, -3.0, -4.0)),
+      ("b", Array(0.0, -1.0, -2.0)),
+      ("c", Array(0.0, -1.0, -2.0, -3.0))
+    )
 
-    val oof_enc = in_cols
-            .zipWithIndex.map {
-              case (col, idx) => (col, (1 until 2).map(
-                i => (idx * i * 10 until idx * i * 10 + 4).map(_.toDouble).toArray).toArray
-              )
-            }
-            .toMap
+    val oof_enc = Map(
+      ("a", Array(
+        Array(0.0, 10.0, 20.0, 30.0, 40.0),
+        Array(0.0, 11.0, 12.0, 13.0, 14.0),
+        Array(0.0, 21.0, 22.0, 23.0, 24.0)
+      )),
+      ("b", Array(
+        Array(0.0, 10.0, 20.0),
+        Array(0.0, 11.0, 12.0),
+        Array(0.0, 21.0, 22.0))
+      ),
+      ("c", Array(
+        Array(0.0, 10.0, 20.0, 30.0),
+        Array(0.0, 11.0, 12.0, 13.0),
+        Array(0.0, 21.0, 22.0, 23.0)
+      ))
+    )
 
     val fold_column = "fold"
+    val id_column = "id"
 
-    // fold_column, some_other_col, a, b, c
-    val data = (1 until 10).map(_ => Row(Seq(0, 42, 1, 1, 1))).toList.asJava
+    // id, fold_column, some_other_col, a, b, c
+    val data = Seq(
+      Row(Seq(0, 0, 42, 1, 1, 1)),
+      Row(Seq(1, 0, 43, 2, 1, 3)),
+      Row(Seq(2, 1, 44, 1, 2, 3)),
+      Row(Seq(3, 1, 45, 1, 2, 2)),
+      Row(Seq(4, 2, 46, 3, 1, 1)),
+      Row(Seq(5, 2, 47, 4, 1, 2)),
+    ).toList.asJava
+
+    val result_enc = Seq(
+      Row(Seq(0, 0, 42, -1, -1, -1)),
+      Row(Seq(1, 0, 43, -2, -1, -3)),
+      Row(Seq(2, 1, 44, -1, -2, -3)),
+      Row(Seq(3, 1, 45, -1, -2, -2)),
+      Row(Seq(4, 2, 46, -3, -1, -1)),
+      Row(Seq(5, 2, 47, -4, -1, -2)),
+    )
+
+    val result_oof_enc = Seq(
+      Row(Seq(0, 0, 42, 10, 10, 10)),
+      Row(Seq(1, 0, 43, 20, 10, 20)),
+      Row(Seq(2, 1, 44, 11, 12, 13)),
+      Row(Seq(3, 1, 45, 11, 12, 12)),
+      Row(Seq(4, 2, 46, 23, 21, 21)),
+      Row(Seq(5, 2, 47, 24, 21, 22)),
+    )
+
     val schema = StructType(
-      Array(StructField(fold_column, IntegerType), StructField("some_other_col", IntegerType))
+      Array(
+        StructField(id_column, IntegerType),
+        StructField(fold_column, IntegerType),
+        StructField("some_other_col", IntegerType)
+      )
       ++ in_cols.map(col => StructField(col, IntegerType))
     )
+
+    def checkResult(tdf: DataFrame, df: DataFrame, target_data: Seq[Row]): Unit = {
+      tdf.columns should contain allElementsOf df.columns
+      tdf.columns should contain allElementsOf out_cols
+      out_cols.foreach(col => tdf.schema(col) shouldBe a [DoubleType])
+
+      val resul_rows = tdf.orderBy(id_column).collect()
+      resul_rows.zip(target_data).foreach {
+        case (row, target) => row.toSeq should equal (target.toSeq)
+      }
+    }
 
     val df = spark.createDataFrame(data, schema)
 
@@ -108,30 +173,15 @@ class TestLAMLStringIndexer extends BaseFunSuite {
             .setInputCols(in_cols)
             .setOutputCols(out_cols)
 
-    val tdf = te.transform(df)
+    checkResult(te.transform(df), df, result_oof_enc)
+    checkResult(te.transform(df), df, result_enc)
+    checkResult(te.transform(df), df, result_enc)
 
-    tdf.columns should contain allElementsOf df.columns
-    tdf.columns should contain allElementsOf out_cols
-    out_cols.foreach(col => tdf.schema(col) shouldBe a [DoubleType])
+    val path = workdir + "target_encoder.transformer"
+    te.save(path)
 
-    // TODO: content checking
-
-    val tdf_2 = te.transform(df)
-
-    tdf_2.columns should contain allElementsOf df.columns
-    tdf_2.columns should contain allElementsOf out_cols
-    out_cols.foreach(col => tdf_2.schema(col) shouldBe a [DoubleType])
-
-    // TODO: content checking
-
-    val tdf_3 = te.transform(df)
-
-    tdf_3.columns should contain allElementsOf df.columns
-    tdf_3.columns should contain allElementsOf out_cols
-    out_cols.foreach(col => tdf_3.schema(col) shouldBe a [DoubleType])
-
-    // TODO: content checking
-
-    // TODO: saving loading
+    val loaded_te = TargetEncoderTransformer.load(path)
+    checkResult(loaded_te.transform(df), df, result_enc)
+    checkResult(loaded_te.transform(df), df, result_enc)
   }
 }
