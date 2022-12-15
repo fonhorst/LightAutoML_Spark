5a448b98669cfe8e579b576340e6ecbc1ef1a9e5
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-12-16 01:20:43 +0300

    added simplified scenario for lightgbm parallel

--- examples/spark/spark-ml-pipe-lgb-light-parallel.py
+++ examples/spark/spark-ml-pipe-lgb-light-parallel.py
@@ -16,7 +16,7 @@ from sparklightautoml.utils import logging_config, VERBOSE_LOGGING_FORMAT, log_e
 from sparklightautoml.validation.iterators import SparkFoldsIterator
 
 logging.config.dictConfig(logging_config(level=logging.INFO, log_filename='/tmp/slama.log'))
-logging.basicConfig(level=logging.DEBUG, format=VERBOSE_LOGGING_FORMAT)
+logging.basicConfig(level=logging.INFO, format=VERBOSE_LOGGING_FORMAT)
 logger = logging.getLogger(__name__)
 
 # NOTE! This demo requires datasets to be downloaded into a local folder.
@@ -48,43 +48,27 @@ if __name__ == "__main__":
         score = task.get_dataset_metric()
 
         sreader = SparkToSparkReader(task=task, cv=cv, advanced_roles=False)
-        sdataset = sreader.fit_read(train_df, roles=roles, persistence_manager=persistence_manager)
-
-        iterator = SparkFoldsIterator(sdataset)#.convert_to_holdout_iterator()
-
         spark_ml_algo = SparkBoostLGBM(freeze_defaults=False, use_single_dataset_mode=False, parallelism=2)
         spark_features_pipeline = SparkLGBSimpleFeatures()
 
-        ml_pipe = SparkMLPipeline(
-            ml_algos=[spark_ml_algo],
-            pre_selection=None,
-            features_pipeline=spark_features_pipeline,
-            post_selection=None
-        )
+        sdataset = sreader.fit_read(train_df, roles=roles, persistence_manager=persistence_manager)
+        sdataset = spark_features_pipeline.fit_transform(sdataset)
 
-        oof_preds_ds = ml_pipe.fit_predict(iterator)
+        sdataset.data.write.parquet("/opt/slama_data/train.parquet", mode='overwrite')
+
+        iterator = SparkFoldsIterator(sdataset).convert_to_holdout_iterator()
+        oof_preds_ds = spark_ml_algo.fit_predict(iterator)
         oof_score = score(oof_preds_ds[:, spark_ml_algo.prediction_feature])
         logger.info(f"OOF score: {oof_score}")
 
-        # 1. first way (LAMA API)
         test_sds = sreader.read(test_df, add_array_attrs=True)
-        test_preds_ds = ml_pipe.predict(test_sds)
+        test_sds = spark_features_pipeline.fit_transform(test_sds)
+
+        sdataset.data.write.parquet("/opt/slama_data/test.parquet", mode='overwrite')
+
+        test_preds_ds = spark_ml_algo.predict(test_sds)
         test_score = score(test_preds_ds[:, spark_ml_algo.prediction_feature])
         logger.info(f"Test score (#1 way): {test_score}")
-
-        # # 2. second way (Spark ML API, save-load-predict)
-        # transformer = PipelineModel(stages=[sreader.transformer(add_array_attrs=True), ml_pipe.transformer()])
-        # transformer.write().overwrite().save("/tmp/reader_and_spark_ml_pipe_lgb")
-        #
-        # pipeline_model = PipelineModel.load("/tmp/reader_and_spark_ml_pipe_lgb")
-        # test_pred_df = pipeline_model.transform(test_df)
-        # test_pred_df = test_pred_df.select(
-        #     SparkDataset.ID_COLUMN,
-        #     F.col(roles['target']).alias('target'),
-        #     F.col(spark_ml_algo.prediction_feature).alias('prediction')
-        # )
-        # test_score = score(test_pred_df)
-        # logger.info(f"Test score (#3 way): {test_score}")
 
     logger.info("Finished")
 
