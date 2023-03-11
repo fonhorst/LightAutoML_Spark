3740d9318fe8a184db16e89a5c2bb2d890c09cbe
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-03-11 15:06:43 +0300

    ---

--- scala-lightautoml-transformers/src/test/scala/org/apache/spark/lightautoml/utils/TestAveragingTransformer.scala
+++ scala-lightautoml-transformers/src/test/scala/org/apache/spark/lightautoml/utils/TestAveragingTransformer.scala
@@ -4,12 +4,13 @@ import breeze.linalg
 import org.apache.spark.internal.Logging
 import org.apache.spark.ml.linalg.{DenseVector, Vector, Vectors}
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.functions.{array, col, udf}
+import org.apache.spark.sql.functions.{array, col, udf, arrays_zip, explode, abs}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import org.apache.spark.ml.functions.vector_to_array
 import org.apache.spark.sql.types.{DataTypes, StructField, StructType, ArrayType, DoubleType}
 import scala.collection.JavaConverters._
+import org.apache.spark.ml.functions.{array_to_vector, vector_to_array}
 
 class TestAveragingTransformer extends AnyFunSuite with BeforeAndAfterAll with Logging {
   val num_workers = 3
@@ -51,25 +52,26 @@ class TestAveragingTransformer extends AnyFunSuite with BeforeAndAfterAll with L
   test("AveragingTransformer") {
     val numPreds = 5
     val dimSize = 3
-    val fields = StructField(s"correct_answer", ArrayType(DataTypes.DoubleType)) :: (0 until numPreds)
+    val fields = StructField(s"id", DataTypes.IntegerType) :: StructField(s"correct_answer", ArrayType(DataTypes.DoubleType)) :: (0 until numPreds)
             .map(i => StructField(s"pred_$i", ArrayType(DataTypes.DoubleType))).toList
     val schema = StructType(fields)
     val data = List(
-      Row(Array(0.38, 0.23, 0.39), Array(0.38, 0.23, 0.39), null, null, null, null),
-      Row(Array(0.38, 0.23, 0.39), null, null, Array(0.38, 0.23, 0.39), null, null),
-      Row(null, null, null, null, null, null),
-      Row(Array(0.445, 0.255, 0.3), Array(0.38, 0.23, 0.39), Array(0.21, 0.48, 0.31), Array(0.99, 0.01, 0.0), Array(0.2, 0.3, 0.5), null),
-      Row(Array(0.4022, 0.3386, 0.2592),Array(0.38, 0.23, 0.39), Array(0.21, 0.48, 0.31), Array(0.99, 0.01, 0.0), Array(0.2, 0.3, 0.5), Array(0.231, 0.673, 0.096)),
+      Row(0, Array(0.38, 0.23, 0.39), Array(0.38, 0.23, 0.39), null, null, null, null),
+      Row(2, Array(0.38, 0.23, 0.39), null, null, Array(0.38, 0.23, 0.39), null, null),
+      Row(3, null, null, null, null, null, null),
+      Row(4, Array(0.445, 0.255, 0.3), Array(0.38, 0.23, 0.39), Array(0.21, 0.48, 0.31), Array(0.99, 0.01, 0.0), Array(0.2, 0.3, 0.5), null),
+      Row(5, Array(0.4022, 0.3386, 0.2592),Array(0.38, 0.23, 0.39), Array(0.21, 0.48, 0.31), Array(0.99, 0.01, 0.0), Array(0.2, 0.3, 0.5), Array(0.231, 0.673, 0.096)),
     ).asJava
 
+    val cols = (0 until numPreds).map(i => col(s"pred_$i"))
+    val vec_cols = array(cols.map(array_to_vector): _*)
+
     val df = spark.createDataFrame(data, schema)
+            .select($"id", $"correct_answer", vector_to_array(vectorUdf(vec_cols)).alias("pred"))
 
-//    val df = spark.read.parquet("/opt/tmp/not_averaged_dataset.parquet")
+    val checks_df = df.select($"id", explode(arrays_zip($"correct_answer", $"pred")).alias("zipped"))
+            .select($"id", $"zipped", (abs($"zipped.correct_answer" - $"zipped.pred") < 0.00001).alias("is_correct"))
 
-    val cols = (0 until numPreds).map(i => col(s"Mod_0_LightGBM_prediction_$i"))
-
-    val new_df = df.select($"correct_answer", vectorUdf(array(cols:_*)).alias("avg_predict"))
-    df.write.mode("overwrite").format("noop").save()
-    val k = 0
+    assert(checks_df.count() == checks_df.where($"is_correcy").count())
   }
 }
