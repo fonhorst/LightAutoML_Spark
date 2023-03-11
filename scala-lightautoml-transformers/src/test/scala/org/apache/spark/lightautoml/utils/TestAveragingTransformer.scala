34fbb9cc7006585f732d91020862d9565fac62b5
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-03-11 15:37:04 +0300

    working test of the averager

--- scala-lightautoml-transformers/src/test/scala/org/apache/spark/lightautoml/utils/TestAveragingTransformer.scala
+++ scala-lightautoml-transformers/src/test/scala/org/apache/spark/lightautoml/utils/TestAveragingTransformer.scala
@@ -2,15 +2,18 @@ package org.apache.spark.lightautoml.utils
 
 import breeze.linalg
 import org.apache.spark.internal.Logging
+import org.apache.spark.ml
 import org.apache.spark.ml.linalg.{DenseVector, Vector, Vectors}
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.functions.{array, col, udf, arrays_zip, explode, abs}
+import org.apache.spark.sql.functions.{abs, array, arrays_zip, col, explode, isnull, lit, udf, when}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import org.apache.spark.ml.functions.vector_to_array
-import org.apache.spark.sql.types.{DataTypes, StructField, StructType, ArrayType, DoubleType}
+import org.apache.spark.sql.types.{ArrayType, DataTypes, DoubleType, StructField, StructType}
+
 import scala.collection.JavaConverters._
 import org.apache.spark.ml.functions.{array_to_vector, vector_to_array}
+import org.apache.spark.ml.linalg.VectorUDT
 
 class TestAveragingTransformer extends AnyFunSuite with BeforeAndAfterAll with Logging {
   val num_workers = 3
@@ -31,7 +34,8 @@ class TestAveragingTransformer extends AnyFunSuite with BeforeAndAfterAll with L
   private val vectorUdf = udf { (vecs: Seq[DenseVector], vec_size: Int) =>
     val not_null_vecs = vecs.count(_ != null)
     if (not_null_vecs == 0){
-      null
+//      null
+      new DenseVector(linalg.DenseVector.fill(vec_size)(0.0).toArray)
     }
     else {
       val result = vecs.map { vec: Any =>
@@ -58,20 +62,21 @@ class TestAveragingTransformer extends AnyFunSuite with BeforeAndAfterAll with L
     val data = List(
       Row(0, Array(0.38, 0.23, 0.39), Array(0.38, 0.23, 0.39), null, null, null, null),
       Row(2, Array(0.38, 0.23, 0.39), null, null, Array(0.38, 0.23, 0.39), null, null),
-      Row(3, null, null, null, null, null, null),
+      Row(3, Array(0.0, 0.0, 0.0), null, null, null, null, null),
       Row(4, Array(0.445, 0.255, 0.3), Array(0.38, 0.23, 0.39), Array(0.21, 0.48, 0.31), Array(0.99, 0.01, 0.0), Array(0.2, 0.3, 0.5), null),
       Row(5, Array(0.4022, 0.3386, 0.2592),Array(0.38, 0.23, 0.39), Array(0.21, 0.48, 0.31), Array(0.99, 0.01, 0.0), Array(0.2, 0.3, 0.5), Array(0.231, 0.673, 0.096)),
     ).asJava
 
     val cols = (0 until numPreds).map(i => col(s"pred_$i"))
-    val vec_cols = array(cols.map(array_to_vector): _*)
+    val vec_cols = array(cols.map(c => when(isnull(c), null).otherwise(array_to_vector(c))): _*)
 
     val df = spark.createDataFrame(data, schema)
-            .select($"id", $"correct_answer", vector_to_array(vectorUdf(vec_cols)).alias("pred"))
+            .select($"id", $"correct_answer", vector_to_array(vectorUdf(vec_cols, lit(dimSize))).alias("pred"))
 
     val checks_df = df.select($"id", explode(arrays_zip($"correct_answer", $"pred")).alias("zipped"))
             .select($"id", $"zipped", (abs($"zipped.correct_answer" - $"zipped.pred") < 0.00001).alias("is_correct"))
 
-    assert(checks_df.count() == checks_df.where($"is_correcy").count())
+    assert(checks_df.count() > 0)
+    assert(checks_df.count() == checks_df.where($"is_correct").count())
   }
 }
