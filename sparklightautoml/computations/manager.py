0fdb33b80d423e86abfede7b8feed4615aefec22
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-03-13 12:28:13 +0300

    add compute slots function

--- sparklightautoml/computations/manager.py
+++ sparklightautoml/computations/manager.py
@@ -1,12 +1,17 @@
 import logging
 import os
+import threading
 from abc import ABC, abstractmethod
+from concurrent.futures.thread import ThreadPoolExecutor
+from contextlib import contextmanager
 from enum import Enum
-from typing import TypeVar, List
+from typing import TypeVar, List, Iterator
 
 from mypy.typeshed.stdlib.multiprocessing.pool import ThreadPool
 from mypy.typeshed.stdlib.typing import Callable, Optional
 from pyspark import inheritable_thread_target
+
+from sparklightautoml.utils import SparkDataFrame
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +20,7 @@ ENV_VAR_SLAMA_COMPUTATIONS_MANAGER = "SLAMA_COMPUTATIONS_MANAGER"
 __computations_manager__: Optional['ComputationsManager'] = None
 
 T = TypeVar("T")
+S = TypeVar("S")
 
 
 def _compute_sequential(tasks: List[Callable[[], T]]) -> List[T]:
@@ -33,40 +39,76 @@ class ComputationsManager(ABC):
         ...
 
     @abstractmethod
-    def compute_exclusively(self, tasks: List[Callable[T]]) -> List[T]:
+    def compute_with_slots(self, name: str, prepare_slots: Callable[List[S]],
+                           tasks: List[Callable[[S], T]], pool_type: PoolType) -> List[T]:
         ...
 
 
 class ParallelComputationsManager(ComputationsManager):
-    def __init__(self, ml_pipes_pool_size: int, ml_algos_pool_size: int, default_pool_size: int):
-        self._ml_pipes_pool: Optional[ThreadPool] = \
-            ThreadPool(processes=ml_pipes_pool_size) if ml_pipes_pool_size > 0 else None
-        self._ml_algos_pool: Optional[ThreadPool] = \
-            ThreadPool(processes=ml_algos_pool_size) if ml_algos_pool_size > 0 else None
-        self._default_pool: Optional[ThreadPool] = \
-            ThreadPool(processes=default_pool_size) if default_pool_size > 0 else None
+    def __init__(self, ml_pipes_pool_size: int = 10, ml_algos_pool_size: int = 20, default_pool_size: int = 10):
+        self._pools ={
+            PoolType.ML_PIPELINES: ThreadPool(processes=ml_pipes_pool_size) if ml_pipes_pool_size > 0 else None,
+            PoolType.ML_ALGOS: ThreadPool(processes=ml_algos_pool_size) if ml_algos_pool_size > 0 else None,
+            PoolType.DEFAULT: ThreadPool(processes=default_pool_size) if default_pool_size > 0 else None
+        }
 
-    def get_pool(self, pool_type: PoolType) -> Optional[ThreadPool]:
-        if pool_type == pool_type.ML_PIPELINES:
-            return self._ml_pipes_pool
-        elif pool_type == pool_type.ML_ALGOS:
-            return self._ml_algos_pool
-        else:
-            return self._default_pool
+        self._pools_lock = threading.Lock()
+
+    def _get_pool(self, pool_type: PoolType) -> Optional[ThreadPool]:
+        return self._pools.get(pool_type, None)
 
     def compute(self, tasks: List[Callable[[], T]], pool_type: PoolType) -> List[T]:
-        pool = self.get_pool(pool_type)
+        # TODO: wouldn't be it a problem
+        pool = self._get_pool(pool_type)
 
         if not pool:
             return _compute_sequential(tasks)
 
-        ptasks = map(inheritable_thread_target, tasks)
-        results = sorted(
-            (result for result in pool.imap_unordered(lambda f: f(), ptasks) if result),
-            key=lambda x: x[0]
-        )
+        with self._pools_lock:
+            ptasks = map(inheritable_thread_target, tasks)
+            results = sorted(
+                (result for result in pool.imap_unordered(lambda f: f(), ptasks) if result),
+                key=lambda x: x[0]
+            )
 
         return results
+
+    def compute_with_slots(self, name: str, prepare_slots: Callable[List[S]],
+                           tasks: List[Callable[[S], T]], pool_type: PoolType) -> List[T]:
+        with self._block_all_pools(pool_type):
+            # check pools
+            assert all((pool is None) for _pool_type, pool in self._pools.items() if _pool_type != pool_type), \
+                f"All thread pools except {pool_type} should be None"
+            pool = self._get_pool(pool_type)
+            # TODO: check the pool is empty
+
+            slots = prepare_slots()
+            slots_lock = threading.Lock()
+
+            @contextmanager
+            def _use_train() -> Iterator[S]:
+                with slots_lock:
+                    free_slot = next((slot for slot in slots if slot.free))
+                    free_slot.free = False
+
+                yield free_slot
+
+                with slots_lock:
+                    free_slot.free = True
+
+            def _func(task):
+                with _use_train() as slot:
+                    return task(slot)
+
+            f_tasks = [_func(task) for task in tasks]
+
+            return self.compute(f_tasks, pool_type.DEFAULT)
+
+    @contextmanager
+    def _block_all_pools(self, pool_type: PoolType):
+        # need to block pool
+        with self._pools_lock:
+            yield self._get_pool(pool_type)
 
 
 class SequentialComputationsManager(ComputationsManager):
